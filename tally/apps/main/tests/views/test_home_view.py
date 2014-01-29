6067426c26f632c0bcc91f929b2405d18c803378
Ukang'a Dickson <ukanga@gmail.com>
2014-01-29 16:14:24 +0300

    UK: refactor home view

--- tally/apps/main/tests/views/test_home_view.py
+++ tally/apps/main/tests/views/test_home_view.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.test import RequestFactory
 
-from tally.apps.main.views.home_view import HomeView
+from tally.apps.main.views.home import HomeView
 
 
 class TestHomeView(TestCase):
--- tally/urls.py
+++ tally/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from tally.apps.main.views.home_view import HomeView
+from tally.apps.main.views.home import HomeView
 
 
 urlpatterns = patterns(
