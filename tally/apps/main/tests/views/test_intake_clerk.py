1b81524dae1c6abb8853ac6b0fdddb1b4e6ebb80
Ukang'a Dickson <ukanga@gmail.com>
2014-01-29 17:43:31 +0300

    UK: add initial work on intake clerk view

---
+++ tally/apps/main/tests/views/test_intake_clerk.py
@@ -0,0 +1,27 @@
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import AnonymousUser
+from django.test import RequestFactory
+
+from tally.apps.main.views.intake_clerk import IntakeClerkView
+from tally.libs.permissions import groups
+from tally.libs.tests.test_base import TestBase
+
+
+class TestIntakeClerkView(TestBase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.view = IntakeClerkView.as_view()
+        self._create_permission_groups()
+
+    def test_home_page(self):
+        request = self.factory.get('/')
+        request.user = AnonymousUser()
+        with self.assertRaises(PermissionDenied):
+            response = self.view(request)
+        self._create_and_login_user()
+        request.user = self.user
+        with self.assertRaises(PermissionDenied):
+            response = self.view(request)
+        self._add_user_to_group(self.user, groups.INTAKE_CLERK)
+        response = self.view(request)
+        self.assertContains(response, '<h1>Intake Dashboard</h1>')
