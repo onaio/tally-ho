1a6143c8497cfd1daa661d3168009cecd3fae943
Ukang'a Dickson <ukanga@gmail.com>
2014-01-29 09:39:57 +0300

    UK: add reversion and south to installed apps

--- tally/settings.py
+++ tally/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reversion',
     'tally.apps.main',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
