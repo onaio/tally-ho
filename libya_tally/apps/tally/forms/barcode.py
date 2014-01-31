ce2bd0575e2c378373739adf43190ee81a6c1769
Ukang'a Dickson <ukanga@gmail.com>
2014-01-31 16:57:25 +0300

    UK: add barcode validation

--- libya_tally/apps/tally/forms/barcode.py
+++ libya_tally/apps/tally/forms/barcode.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.utils.translation import ugettext as _
+
+from libya_tally.apps.tally.models.result_form import ResultForm
 
 
 class IntakeBarcodeForm(forms.Form):
     barcode = forms.CharField(max_length=9, min_length=9)
     barcode_copy = forms.CharField(max_length=9, min_length=9)
+
+    def clean(self):
+        cleaned_data = super(IntakeBarcodeForm, self).clean()
+        barcode = cleaned_data.get('barcode')
+        barcode_copy = cleaned_data.get('barcode_copy')
+
+        if barcode != barcode_copy:
+            raise forms.ValidationError(_(u"Barcodes do not match!"))
+
+        try:
+            ResultForm.objects.get(barcode=barcode)
+        except ResultForm.DoesNotExist:
+            raise forms.ValidationError(u"Barcode does not exist")
+        return cleaned_data
