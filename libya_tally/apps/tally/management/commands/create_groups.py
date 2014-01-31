8e2ceae60c8bfd0b1c8299ad4f7cdd424a00e54b
Ukang'a Dickson <ukanga@gmail.com>
2014-01-31 10:38:25 +0300

    UK: create groups command

---
+++ libya_tally/apps/tally/management/commands/create_groups.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext_lazy
+from libya_tally.libs.permissions.groups import create_permission_groups
+
+
+class Command(BaseCommand):
+    help = ugettext_lazy("Create groups.")
+
+    def handle(self, *args, **kwargs):
+        create_permission_groups()
