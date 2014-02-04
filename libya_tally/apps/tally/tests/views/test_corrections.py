e2c0ade90d5c08ab6934444bf14b9f22a94add54
Ukang'a Dickson <ukanga@gmail.com>
2014-02-04 06:34:15 +0300

    UK: implement send to quality control, corrections match

--- libya_tally/apps/tally/tests/views/test_corrections.py
+++ libya_tally/apps/tally/tests/views/test_corrections.py
@@ -9,8 +9,28 @@ from libya_tally.libs.models.enums.entry_version import EntryVersion
 from libya_tally.libs.models.enums.form_state import FormState
 from libya_tally.libs.permissions import groups
 from libya_tally.libs.tests.test_base import create_result_form, \
-    create_candidate, create_center, create_station, center_data,\
-    result_form_data, result_form_data_blank, TestBase
+    create_candidate, create_center, create_station, TestBase
+
+
+def create_results(result_form, vote1=1, vote2=1):
+    code = '12345'
+    center = create_center(code)
+    create_station(center)
+    ballot = result_form.ballot
+    candidate_name = 'candidate name'
+    candidate = create_candidate(ballot, candidate_name)
+
+    Result.objects.create(
+        candidate=candidate,
+        result_form=result_form,
+        entry_version=EntryVersion.DATA_ENTRY_1,
+        votes=vote1)
+
+    Result.objects.create(
+        candidate=candidate,
+        result_form=result_form,
+        entry_version=EntryVersion.DATA_ENTRY_2,
+        votes=vote2)
 
 
 class TestCorrectionView(TestBase):
@@ -18,7 +38,7 @@ class TestCorrectionView(TestBase):
         self.factory = RequestFactory()
         self._create_permission_groups()
 
-    def _common_view_tests(self, view):
+    def _common_view_tests(self, view, session={}):
         request = self.factory.get('/')
         request.user = AnonymousUser()
         with self.assertRaises(PermissionDenied):
@@ -28,12 +48,13 @@ class TestCorrectionView(TestBase):
         with self.assertRaises(PermissionDenied):
             view(request)
         self._add_user_to_group(self.user, groups.CORRECTIONS_CLERK)
+        request.session = session
         response = view(request)
         response.render()
         self.assertIn('/accounts/logout/', response.content)
         return response
 
-    def test_corrctions_page(self):
+    def test_corrections_page(self):
         response = self._common_view_tests(views.CorrectionView.as_view())
         self.assertContains(response, 'Correction')
         self.assertIn('<form id="result_form"', response.content)
@@ -71,27 +92,8 @@ class TestCorrectionView(TestBase):
 
     def test_corrections_redirects_to_corrections_match(self):
         barcode = '123456789'
-        code = '12345'
-        votes = 1
-        center = create_center(code)
-        create_station(center)
         result_form = create_result_form(form_state=FormState.CORRECTION)
-        ballot = result_form.ballot
-        candidate_name = 'candidate name'
-        candidate = create_candidate(ballot, candidate_name)
-
-        Result.objects.create(
-            candidate=candidate,
-            result_form=result_form,
-            entry_version=EntryVersion.DATA_ENTRY_1,
-            votes=votes)
-
-        Result.objects.create(
-            candidate=candidate,
-            result_form=result_form,
-            entry_version=EntryVersion.DATA_ENTRY_2,
-            votes=votes)
-
+        create_results(result_form, vote1=1, vote2=1)
         self._create_and_login_user()
         self._add_user_to_group(self.user, groups.CORRECTIONS_CLERK)
         view = views.CorrectionView.as_view()
@@ -105,27 +107,8 @@ class TestCorrectionView(TestBase):
 
     def test_corrections_redirects_to_corrections_required(self):
         barcode = '123456789'
-        code = '12345'
-        votes = 1
-        center = create_center(code)
-        create_station(center)
         result_form = create_result_form(form_state=FormState.CORRECTION)
-        ballot = result_form.ballot
-        candidate_name = 'candidate name'
-        candidate = create_candidate(ballot, candidate_name)
-
-        Result.objects.create(
-            candidate=candidate,
-            result_form=result_form,
-            entry_version=EntryVersion.DATA_ENTRY_1,
-            votes=votes)
-
-        Result.objects.create(
-            candidate=candidate,
-            result_form=result_form,
-            entry_version=EntryVersion.DATA_ENTRY_2,
-            votes=3)
-
+        create_results(result_form, vote1=1, vote2=3)
         self._create_and_login_user()
         self._add_user_to_group(self.user, groups.CORRECTIONS_CLERK)
         view = views.CorrectionView.as_view()
@@ -136,3 +119,40 @@ class TestCorrectionView(TestBase):
         response = view(request)
         self.assertEqual(response.status_code, 302)
         self.assertIn('corrections/required', response['location'])
+
+    def test_corrections_match_page(self):
+        result_form = create_result_form(form_state=FormState.CORRECTION)
+        session = {'result_form': result_form.pk}
+        response = self._common_view_tests(
+            views.CorrectionMatchView.as_view(), session=session)
+        string_matches = [
+            'Corrections', 'Form Race Type:', 'Form Entries Match',
+            'Pass to Quality Control', '<input type="hidden" '
+            'name="result_form" value="%s">' % result_form.pk]
+        for check_str in string_matches:
+            self.assertContains(response, check_str)
+
+    def test_corrections_match_pass_to_quality_control(self):
+        view = views.CorrectionMatchView.as_view()
+        result_form = create_result_form(form_state=FormState.CORRECTION)
+        create_results(result_form, vote1=3, vote2=3)
+        self._create_and_login_user()
+        self._add_user_to_group(self.user, groups.CORRECTIONS_CLERK)
+        self.assertEqual(
+            Result.objects.filter(result_form=result_form).count(), 2)
+        session = {'result_form': result_form.pk}
+        data = {'result_form': result_form.pk,
+                'pass_to_quality_control': 'true'}
+        request = self.factory.post('/', data=data)
+        request.session = session
+        request.user = self.user
+        response = view(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            Result.objects.filter(result_form=result_form).count(), 3)
+        self.assertEqual(
+            Result.objects.filter(result_form=result_form,
+                                  entry_version=EntryVersion.FINAL).count(), 1)
+        updated_result_form = ResultForm.objects.get(pk=result_form.pk)
+        self.assertEqual(updated_result_form.form_state,
+                         FormState.QUALITY_CONTROL)
--- libya_tally/apps/tally/views/corrections.py
+++ libya_tally/apps/tally/views/corrections.py
@@ -1,14 +1,18 @@
+from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import ugettext as _
 from django.views.generic import FormView
 
 from libya_tally.apps.tally.forms.intake_barcode_form import\
     IntakeBarcodeForm
+from libya_tally.apps.tally.forms.pass_to_quality_control_form import \
+    PassToQualityControlForm
 from libya_tally.apps.tally.models.result import Result
 from libya_tally.apps.tally.models.result_form import ResultForm
 from libya_tally.libs.models.enums.entry_version import EntryVersion
 from libya_tally.libs.models.enums.form_state import FormState
 from libya_tally.libs.permissions import groups
+from libya_tally.libs.utils.common import session_matches_post_result_form
 from libya_tally.libs.views import mixins
 from libya_tally.libs.views.form_state import form_in_state
 
@@ -63,7 +67,48 @@ class CorrectionView(mixins.GroupRequiredMixin,
 class CorrectionMatchView(mixins.GroupRequiredMixin,
                           mixins.ReverseSuccessURLMixin,
                           FormView):
-    form_class = IntakeBarcodeForm
+    form_class = PassToQualityControlForm
     group_required = groups.CORRECTIONS_CLERK
     template_name = "tally/corrections/match.html"
     success_url = 'corrections'
+
+    def get(self, *args, **kwargs):
+        pk = self.request.session.get('result_form')
+        result_form = get_object_or_404(ResultForm, pk=pk)
+        form_in_state(result_form, [FormState.CORRECTION])
+
+        return self.render_to_response(
+            self.get_context_data(result_form=result_form))
+
+    @transaction.atomic
+    def post(self, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        if form.is_valid():
+            pk = session_matches_post_result_form(
+                form.cleaned_data, self.request)
+            result_form = get_object_or_404(ResultForm, pk=pk)
+            form_in_state(result_form, [FormState.CORRECTION])
+
+            if not match_forms(result_form):
+                raise Exception(_(u"Results do not match."))
+
+            results = Result.objects.filter(
+                result_form=result_form,
+                entry_version=EntryVersion.DATA_ENTRY_2)
+            for result in results:
+                Result.objects.create(
+                    candidate=result.candidate,
+                    result_form=result_form,
+                    entry_version=EntryVersion.FINAL,
+                    votes=result.votes)
+
+            result_form.form_state = FormState.QUALITY_CONTROL
+            result_form.save()
+
+            del self.request.session['result_form']
+
+            return redirect(self.success_url)
+        else:
+            return self.form_invalid(form)
