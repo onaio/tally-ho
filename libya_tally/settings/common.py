12988a8a1edd89d60e82096c3e10a7d79944882a
Ukang'a Dickson <ukanga@gmail.com>
2014-02-05 17:58:19 +0300

    UK: static root

--- libya_tally/settings/common.py
+++ libya_tally/settings/common.py
@@ -102,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # this is default
