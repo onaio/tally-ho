0bdf6541c1d404fbe62cce1c37ba11dbc2581789
Ukang'a Dickson <ukanga@gmail.com>
2014-02-06 07:06:07 +0300

    UK: deplay local_settings file

---
+++ deploy/var/www/tally-system/libya_tally/settings/local_settings.py
@@ -0,0 +1,17 @@
+import os
+from libya_tally.settings.common import *  # flake8: noqa
+
+os.environ.setdefault('DB_NAME', 'REPLACE_DB_NAME')
+os.environ.setdefault('DB_USER', 'REPLACE_DB_USER')
+os.environ.setdefault('DB_PASSWORD', 'REPLACE_DB_PASSWORD')
+os.environ.setdefault('DB_HOST', 'REPLACE_DB_HOST')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':  os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST')
+    }
+}
