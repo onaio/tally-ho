50f2009f9f909ca87ca4ed515104a94781b95d6d
pld <peter@helioid.com>
2014-02-11 07:13:17 -0500

    PLD: move barcode js to a js file

---
+++ libya_tally/apps/tally/static/js/barcode_verify.js
@@ -0,0 +1,70 @@
+function barcodes_match(empty_message, length_message, mismatch_message) {
+    var barcode = document.getElementById('id_barcode');
+    var barcode_copy = document.getElementById('id_barcode_copy');
+
+    if (barcode.value.length == 9 && barcode.value == barcode_copy.value) {
+        barcode_copy.parentNode.setAttribute('class', 'has-success');
+        return true;
+    }
+    barcode_copy.parentNode.setAttribute('class', 'has-error');
+
+    if (barcode.value == "" || barcode_copy.value == "") {
+        barcode.parentNode.setAttribute('class', 'has-error');
+        alert(empty_message);
+    } else if (barcode.value.length < 9) {
+        barcode.parentNode.setAttribute('class', 'has-error');
+        alert(length_message);
+    }
+    else {
+        alert(mismatch_message);
+    }
+
+    return false;
+}
+
+function show_barcode_hide_placeholder() {
+    $("#barcode_placeholder").hide();
+    $("#id_barcode").show();
+}
+
+function hide_barcode_show_placeholder() {
+    $("#barcode_placeholder").show();
+    $("#id_barcode").hide();
+}
+
+$(document).ready(function() {
+    $("#id_barcode").focusout(function(evt) {
+        if ($('#id_barcode').val() != '' && $('#id_barcode_copy').val() != $('#id_barcode').val()) {
+            hide_barcode_show_placeholder()
+        }
+    });
+
+    $("#barcode_placeholder").focusin(function(evt) {
+        show_barcode_hide_placeholder();
+        $("#id_barcode").focus();
+    });
+
+    $("#id_barcode_copy").focusout(function(evt) {
+        if ($("#id_barcode_copy").val() != '' && $('#id_barcode').val() != $("#id_barcode_copy").val()) {
+            show_barcode_hide_placeholder();
+        }
+    });
+
+    $("#id_barcode_copy").keyup(function(evt) {
+        if ($("#id_barcode_copy").val().length == 9 && $("#id_barcode").val() == $("#id_barcode_copy").val()) {
+            show_barcode_hide_placeholder();
+            $("#id_barcode").parent().addClass('has-success');
+            $("#id_barcode_copy").parent().addClass('has-success');
+        } else if ($("#id_barcode_copy").val().length == 9) {
+            show_barcode_hide_placeholder();
+            $("#id_barcode").parent().addClass('has-error');
+            $("#id_barcode_copy").parent().addClass('has-error');
+        } else {
+            hide_barcode_show_placeholder();
+        }
+    });
+
+    $("#id_barcode_copy").focusin(function(evt) {
+        hide_barcode_show_placeholder();
+    });
+});
