213e28ec46aea7f773abb29b4627c7f56c34b6fc
Ukang'a Dickson <ukanga@gmail.com>
2014-02-15 17:41:37 +0200

    UK: tabindex on results form

--- libya_tally/apps/tally/views/data_entry_clerk.py
+++ libya_tally/apps/tally/views/data_entry_clerk.py
@@ -39,11 +39,15 @@ def get_formset_and_candidates(result_form, post_data=None):
     forms_and_candidates = []
 
     last_race_type = None
+    tabindex = 200
     for i, f in enumerate(formset):
         candidate = candidates[i]
         race_type = candidate.race_type_name if\
             candidate.race_type != last_race_type else None
         last_race_type = candidate.race_type
+
+        f.fields['votes'].widget.attrs['tabindex'] = tabindex
+        tabindex += 10
 
         entry = [race_type, f, candidate]
         forms_and_candidates.append(entry)
