e78f232212029174fb81294ee480c85336893084
Ukang'a Dickson <ukanga@gmail.com>
2014-02-16 23:43:43 +0200

    UK: import staff list command, closes #67

---
+++ libya_tally/apps/tally/management/commands/import_staff_list.py
@@ -0,0 +1,89 @@
+import codecs
+import csv
+
+from django.contrib.auth.models import Group, User
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext_lazy
+
+from libya_tally.libs.permissions import groups
+
+STAFF_LIST_PATH = 'data/staff_list.csv'
+
+STAFF_ROLE_DICT = {
+    'ADMINISTRATOR': groups.ADMINISTRATOR,
+    'ARCHIVE SUPERVISOR': groups.ARCHIVE_SUPERVISOR,
+    'ARCHIVE CLERK': groups.ARCHIVE_CLERK,
+    'AUDIT CLERK': groups.AUDIT_CLERK,
+    'CLEARANCE CLERK': groups.CLEARANCE_CLERK,
+    'CLEARANCE SUPERVISOR': groups.CLEARANCE_SUPERVISOR,
+    'CORRECTION CLERK': groups.CORRECTIONS_CLERK,
+    'DATA ENTRY 1 CLERK': groups.DATA_ENTRY_1_CLERK,
+    'DATA ENTRY 2 CLERK': groups.DATA_ENTRY_2_CLERK,
+    'INTAKE CLERK': groups.INTAKE_CLERK,
+    'INTAKE SUPERVISOR': groups.INTAKE_SUPERVISOR,
+    'QUALITY CONTROL CLERK': groups.QUALITY_CONTROL_CLERK,
+    'SUPER ADMINISTRATOR': groups.SUPER_ADMINISTRATOR,
+}
+
+STAFF_ROLES = STAFF_ROLE_DICT.keys()
+
+
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
+
+class Command(BaseCommand):
+    help = ugettext_lazy("Import Staff list.")
+
+    def handle(self, *args, **kwargs):
+        self.import_staff_list()
+
+    def import_staff_list(self):
+        with codecs.open(STAFF_LIST_PATH, encoding='utf-8') as f:
+            reader = unicode_csv_reader(f)
+            reader.next()  # ignore header
+
+            for row in reader:
+                try:
+                    name = row[0]
+                    username = row[1]
+                    role = row[2]
+                except Exception as e:
+                    print "Unable to add user in row: %s. Exception %s." % \
+                        (row, e)
+                else:
+                    try:
+                        first_name = name
+                        last_name = u''
+                        split_names = name.split(u' ')
+                        if len(split_names) > 1:
+                            first_name = split_names[0]
+                            last_name = u' '.join(split_names[1:])
+                        user = User.objects.create_user(
+                            username, password=username,
+                            first_name=first_name,
+                            last_name=last_name
+                        )
+                    except Exception as e:
+                        print "User %s not created! %s" % (username, e)
+                        continue
+                    else:
+                        role = role.upper()
+                        if role in STAFF_ROLES:
+                            group, created = Group.objects.get_or_create(
+                                name=STAFF_ROLE_DICT.get(role))
+                            user.groups.add(group)
+                        else:
+                            print (
+                                "Unable to add user %s to unknown group %s."
+                                % (username, row[2]))
