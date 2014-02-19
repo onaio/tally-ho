723847b0d4f482d8fc6ba8a1602a7e3470958122
pld <peter@helioid.com>
2014-02-19 06:59:30 -0500

    PLD: update expected field name

--- libya_tally/apps/tally/views/corrections.py
+++ libya_tally/apps/tally/views/corrections.py
@@ -43,9 +43,9 @@ def _save_final_recon_form(updated, user, result_form):
 
 
 def incorrect_checks(post_data, result_form, success_url):
-    if 'reject' in post_data:
+    if 'reject_submit' in post_data:
         result_form.reject()
-    elif 'abort' in post_data:
+    elif 'abort_submit' in post_data:
         pass
     else:
         raise SuspiciousOperation('Unknown POST response type')
