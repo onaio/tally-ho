90d3087c5f69aa7785903bd4c071f0b63ce192ed
pld <peter@helioid.com>
2014-02-19 06:05:52 -0500

    f

--- libya_tally/libs/utils/context_processors.py
+++ libya_tally/libs/utils/context_processors.py
@@ -3,6 +3,14 @@ from django.contrib.sites.models import Site
 from django.utils import translation
 
 
+def debug(request):
+    return {'debug': getattr(settings, 'DEBUG', False)}
+
+
+def locale(request):
+    return {'locale': translation.get_language_from_request(request)}
+
+
 def site_name(request):
     site_id = getattr(settings, 'SITE_ID', None)
     try:
@@ -12,7 +20,3 @@ def site_name(request):
     else:
         site_name = site.name
     return {'SITE_NAME': site_name}
-
-
-def locale(request):
-    return {'locale': translation.get_language_from_request(request)}
