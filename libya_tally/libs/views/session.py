860a0d7839d3e59da6492ed5a4d079007c4b766b
pld <peter@helioid.com>
2014-02-20 07:39:16 -0500

    PLD: catch value error from post data

--- libya_tally/libs/views/session.py
+++ libya_tally/libs/views/session.py
@@ -5,10 +5,13 @@ from django.utils.translation import ugettext as _
 def session_matches_post_result_form(post_data, request):
     pk = request.session.get('result_form')
 
-    if 'result_form' not in post_data:
+    try:
+        if 'result_form' not in post_data:
+            raise SuspiciousOperation(_(u"Error: Missing result form!"))
+        elif int(post_data['result_form']) != pk:
+            raise SuspiciousOperation(
+                _(u"Session result_form does not match submitted data."))
+    except ValueError:
         raise SuspiciousOperation(_(u"Error: Missing result form!"))
-    elif int(post_data['result_form']) != pk:
-        raise SuspiciousOperation(
-            _(u"Session result_form does not match submitted data."))
 
     return pk
