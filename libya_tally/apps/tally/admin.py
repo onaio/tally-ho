dd712a30065ce8fb5d3e96a7a4e88800ea0a0dfe
Ukang'a Dickson <ukanga@gmail.com>
2014-02-21 10:02:01 +0200

    UK: list display search filter on userprofileadmin model

--- libya_tally/apps/tally/admin.py
+++ libya_tally/apps/tally/admin.py
@@ -4,6 +4,9 @@ from libya_tally.apps.tally.models.user_profile import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('username', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
 
 admin.site.register(UserProfile, UserProfileAdmin)
