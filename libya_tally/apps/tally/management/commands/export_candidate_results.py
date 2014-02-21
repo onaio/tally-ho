d191bdc13d04549827831b83c1ec63ea2ae28ab7
pld <peter@helioid.com>
2014-02-21 10:22:03 -0500

    PLD: order candidates by number of votes

--- libya_tally/apps/tally/management/commands/export_candidate_results.py
+++ libya_tally/apps/tally/management/commands/export_candidate_results.py
@@ -11,6 +11,11 @@ from libya_tally.libs.models.enums.form_state import FormState
 OUTPUT_PATH = 'results/candidate_votes.csv'
 
 
+def distinct_forms(ballot):
+    return ballot.resultform_set.filter(center__isnull=False).distinct(
+        'center__id', 'station_number', 'ballot__id')
+
+
 def get_votes(candidate):
     """Return the active results for this candidate that are for archived
     result forms."""
@@ -49,9 +54,11 @@ class Command(BaseCommand):
             w.writeheader()
 
             for ballot in Ballot.objects.all():
-                forms = ballot.resultform_set.filter(
-                    center__isnull=False).distinct(
-                    'center__id', 'station_number', 'ballot__id')
+                forms = distinct_forms(ballot)
+
+                if not forms:
+                    forms = distinct_forms(
+                        ballot.sc_component.all()[0].ballot_general)
 
                 num_stations = forms.count()
                 num_stations_completed = forms.filter(
@@ -62,7 +69,7 @@ class Command(BaseCommand):
                     ipdb.set_trace()
 
                 percent_complete = round(
-                    100 * num_stations_completed / num_stations, 2)
+                    100 * num_stations_completed / num_stations, 3)
 
                 output = OrderedDict({
                     'ballot number': ballot.number,
@@ -70,11 +77,20 @@ class Command(BaseCommand):
                     'stations completed': num_stations_completed,
                     'stations percent completed': percent_complete})
 
+                candidates_to_votes = {}
                 for candidate in ballot.candidates.all():
                     votes = get_votes(candidate)
-                    output['candidate %s name' % candidate.order] =\
-                        candidate.full_name
-                    output['candidate %s votes' % candidate.order] = votes
+                    candidates_to_votes[candidate.full_name] = votes
+
+                candidates_to_votes = OrderedDict((sorted(
+                    candidates_to_votes.items(), key=lambda t: t[1],
+                    reverse=True)))
+
+                for i, item in enumerate(candidates_to_votes.items()):
+                    candidate, votes = item
+
+                    output['candidate %s name' % (i + 1)] = candidate
+                    output['candidate %s votes' % (i + 1)] = votes
 
                 w.writerow({k: v.encode('utf8') if isinstance(v, basestring)
                             else v for k, v in output.items()})
