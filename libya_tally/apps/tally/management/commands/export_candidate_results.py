96438f4417c47c2de0ff602e77df87fe53596de5
pld <peter@helioid.com>
2014-02-23 06:01:40 -0500

    PLD: abstract candidates

--- libya_tally/apps/tally/management/commands/export_candidate_results.py
+++ libya_tally/apps/tally/management/commands/export_candidate_results.py
@@ -38,7 +38,7 @@ def save_barcode_results(complete_barcodes):
             result_form = ResultForm.objects.get(barcode=barcode)
             candidates = result_form.ballot.candidates.all()
 
-            for candidate in candidates:
+            for candidate in candidates.order_by('order'):
                 results = candidate.results.filter(
                     result_form=result_form,
                     entry_version=EntryVersion.FINAL,
@@ -59,88 +59,86 @@ def save_barcode_results(complete_barcodes):
                     else v for k, v in output.items()})
 
 
+def export_candidate_votes(output=None):
+    header = ['ballot number',
+              'stations',
+              'stations completed',
+              'stations percent completed']
+
+    max_candidates = 0
+    for ballot in Ballot.objects.all():
+        if ballot.candidates.count() > max_candidates:
+            max_candidates = ballot.candidates.count()
+
+    for i in xrange(1, max_candidates + 1):
+        header.append('candidate %s name' % i)
+        header.append('candidate %s votes' % i)
+
+    complete_barcodes = []
+
+    with open(OUTPUT_PATH, 'wb') as f:
+        # BOM, Excel needs it to open UTF-8 file properly
+        f.write(u'\ufeff'.encode('utf8'))
+        w = csv.DictWriter(f, header)
+        w.writeheader()
+
+        for ballot in Ballot.objects.exclude(number=54):
+            forms = distinct_forms(ballot)
+            final_forms = forms.filter(form_state=FormState.ARCHIVED)
+
+            if not SPECIAL_BALLOTS or ballot.number in SPECIAL_BALLOTS:
+                complete_barcodes.extend([r.barcode for r in final_forms])
+
+            if not forms:
+                forms = distinct_forms(
+                    ballot.sc_component.all()[0].ballot_general)
+
+            num_stations = forms.count()
+            num_stations_completed = final_forms.count()
+
+            percent_complete = round(
+                100 * num_stations_completed / num_stations, 3)
+
+            output = OrderedDict({
+                'ballot number': ballot.number,
+                'stations': num_stations,
+                'stations completed': num_stations_completed,
+                'stations percent completed': percent_complete})
+
+            candidates_to_votes = {}
+            num_results_ary = []
+
+            for candidate in ballot.candidates.all():
+                num_results, votes = get_votes(candidate)
+                candidates_to_votes[candidate.full_name] = votes
+                num_results_ary.append(num_results)
+
+            assert len(set(num_results_ary)) <= 1
+
+            for num_results in num_results_ary:
+                if num_stations_completed != num_results:
+                    print ('[WARNING] Number stations complete (%s) not '
+                           'equal to num_results (%s)' % (
+                               num_stations_completed, num_results))
+
+            candidates_to_votes = OrderedDict((sorted(
+                candidates_to_votes.items(), key=lambda t: t[1],
+                reverse=True)))
+
+            for i, item in enumerate(candidates_to_votes.items()):
+                candidate, votes = item
+
+                output['candidate %s name' % (i + 1)] = candidate
+                output['candidate %s votes' % (i + 1)] = votes
+
+            w.writerow({k: v.encode('utf8') if isinstance(v, basestring)
+                        else v for k, v in output.items()})
+
+        save_barcode_results(complete_barcodes)
+
+
 class Command(BaseCommand):
     help = ugettext_lazy("Export candidate votes list.")
 
     def handle(self, *args, **kwargs):
-        self.export_candidate_votes()
-
-    def export_candidate_votes(self):
-        header = ['ballot number',
-                  'stations',
-                  'stations completed',
-                  'stations percent completed']
-
-        max_candidates = 0
-        for ballot in Ballot.objects.all():
-            if ballot.candidates.count() > max_candidates:
-                max_candidates = ballot.candidates.count()
-
-        for i in xrange(1, max_candidates + 1):
-            header.append('candidate %s name' % i)
-            header.append('candidate %s votes' % i)
-
-        complete_barcodes = []
-
-        with open(OUTPUT_PATH, 'wb') as f:
-            # BOM, Excel needs it to open UTF-8 file properly
-            f.write(u'\ufeff'.encode('utf8'))
-            w = csv.DictWriter(f, header)
-            w.writeheader()
-
-            for ballot in Ballot.objects.exclude(number=54):
-                forms = distinct_forms(ballot)
-                final_forms = forms.filter(form_state=FormState.ARCHIVED)
-
-                if not SPECIAL_BALLOTS or ballot.number in SPECIAL_BALLOTS:
-                    complete_barcodes.extend([r.barcode for r in final_forms])
-
-                if not forms:
-                    forms = distinct_forms(
-                        ballot.sc_component.all()[0].ballot_general)
-
-                num_stations = forms.count()
-                num_stations_completed = final_forms.count()
-
-                percent_complete = round(
-                    100 * num_stations_completed / num_stations, 3)
-
-                output = OrderedDict({
-                    'ballot number': ballot.number,
-                    'stations': num_stations,
-                    'stations completed': num_stations_completed,
-                    'stations percent completed': percent_complete})
-
-                candidates_to_votes = {}
-                num_results_ary = []
-
-                for candidate in ballot.candidates.all():
-                    num_results, votes = get_votes(candidate)
-                    candidates_to_votes[candidate.full_name] = votes
-                    num_results_ary.append(num_results)
-
-                assert len(set(num_results_ary)) <= 1
-
-                for num_results in num_results_ary:
-                    if num_stations_completed != num_results:
-                        print ('[WARNING] Number stations complete (%s) not '
-                               'equal to num_results (%s)' % (
-                                   num_stations_completed, num_results))
-#                        import ipdb
-#                        ipdb.set_trace()
-#                    assert num_stations_completed == num_results
-
-                candidates_to_votes = OrderedDict((sorted(
-                    candidates_to_votes.items(), key=lambda t: t[1],
-                    reverse=True)))
-
-                for i, item in enumerate(candidates_to_votes.items()):
-                    candidate, votes = item
-
-                    output['candidate %s name' % (i + 1)] = candidate
-                    output['candidate %s votes' % (i + 1)] = votes
-
-                w.writerow({k: v.encode('utf8') if isinstance(v, basestring)
-                            else v for k, v in output.items()})
-
-            save_barcode_results(complete_barcodes)
+        export_candidate_votes()
