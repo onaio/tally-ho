ff523106fa6bf5ac7b200e4b65448948b251b4c4
pld <peter@helioid.com>
2014-02-24 03:37:03 -0500

    PLD: import from a different file, remove station import

--- libya_tally/apps/tally/management/commands/import_resultforms.py
+++ libya_tally/apps/tally/management/commands/import_resultforms.py
@@ -12,13 +12,11 @@ from libya_tally.apps.tally.models.ballot import Ballot
 from libya_tally.apps.tally.models.center import Center
 from libya_tally.apps.tally.models.office import Office
 from libya_tally.apps.tally.models.result_form import ResultForm
-from libya_tally.apps.tally.models.station import Station
-from libya_tally.apps.tally.models.sub_constituency import SubConstituency
 from libya_tally.libs.models.enums.form_state import FormState
 from libya_tally.libs.models.enums.gender import Gender
 
 STATIONS_PATH = 'data/stations.csv'
-RESULT_FORMS_PATH = 'data/result_forms.csv'
+RESULT_FORMS_PATH = 'data/new_result_forms.csv'
 
 
 class Command(BaseCommand):
@@ -32,44 +30,8 @@ class Command(BaseCommand):
         result_path = result_path \
             if result_path is not None else RESULT_FORMS_PATH
 
-        print '[INFO] import stations'
-        self.import_stations()
-
         print '[INFO] import result forms'
         self.import_result_forms(result_path)
-
-    def import_stations(self):
-        with open(STATIONS_PATH, 'rU') as f:
-            reader = csv.reader(f)
-            reader.next()  # ignore header
-
-            for row in reader:
-                center_code = row[0]
-
-                try:
-                    center = Center.objects.get(code=center_code)
-                except Center.DoesNotExist:
-                    center, created = Center.objects.get_or_create(
-                        code=center_code,
-                        name=row[1])
-
-                try:
-                    # attempt to convert SC to a number
-                    sc_code = int(float(row[2]))
-                    sub_constituency = SubConstituency.objects.get(
-                        code=sc_code)
-                except (SubConstituency.DoesNotExist, ValueError):
-                    print('[WARNING] SubConstituency "%s" does not exist' %
-                          sc_code)
-
-                gender = getattr(Gender, row[4].upper())
-
-                _, created = Station.objects.get_or_create(
-                    center=center,
-                    sub_constituency=sub_constituency,
-                    gender=gender,
-                    registrants=imp.empty_string_to(row[5], None),
-                    station_number=row[3])
 
     def import_result_forms(self, result_forms_path):
         replacement_count = 0
@@ -123,7 +85,9 @@ class Command(BaseCommand):
 
                 try:
                     form = ResultForm.objects.get(barcode=barcode)
+                    print '[INFO] Found with barcode: %s' % barcode
                 except ResultForm.DoesNotExist:
+                    print '[INFO] Create with barcode: %s' % barcode
                     ResultForm.objects.create(**kwargs)
                 else:
                     if is_replacement:
