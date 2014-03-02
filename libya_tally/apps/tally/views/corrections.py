54a3d9f806a6e534d70295b82008f53a98087f8a
pld <peter@helioid.com>
2014-03-02 09:16:43 -0500

    PLD: cleanup and comments

--- libya_tally/apps/tally/views/corrections.py
+++ libya_tally/apps/tally/views/corrections.py
@@ -27,14 +27,22 @@ from libya_tally.libs.views.corrections import get_matched_forms,\
 from libya_tally.libs.views.form_state import form_in_state, safe_form_in_state
 
 
-def _get_recon_form_dict(result_form):
+def get_recon_form_dict(result_form):
     recon_forms = result_form.reconciliationform_set.filter(active=True)
     recon_form1 = recon_form_for_version(
         recon_forms, EntryVersion.DATA_ENTRY_1)
+
     return {f.name: f.value() for f in recon_form1}
 
 
-def _save_final_recon_form(updated, user, result_form):
+def save_final_recon_form(updated, user, result_form):
+    """Set and save values in a final reconciliation form.
+
+    :param updated: A key value dict of values for the reconciliation form.
+    :param user: The user to associate with the reconciliation form.
+    :param result_form: The result form to associate with this reconciliation
+        form.
+    """
     recon_form_final = ReconciliationForm(**updated)
     recon_form_final.user = user
     recon_form_final.result_form = result_form
@@ -43,6 +51,15 @@ def _save_final_recon_form(updated, user, result_form):
 
 
 def incorrect_checks(post_data, result_form, success_url):
+    """Perform non-success operations on a result form give the post data.
+
+    :param post_data: Data to determine the appropriate action for.
+    :param result_form: Result form to reject if a rejection.
+    :param success_url: Url to redirect to.
+
+    :raises: `SuspiciousOperation` if the post data is an unexpected value.
+    :returns: A redirect to the success url.
+    """
     if 'reject_submit' in post_data:
         result_form.reject()
     elif 'abort_submit' in post_data:
@@ -54,21 +71,34 @@ def incorrect_checks(post_data, result_form, success_url):
 
 
 def get_corrections_forms(result_form):
+    """Return a list of corrections forms for the reconciliation and each
+    result type.
+
+    :param result_form: The result form to fetch results for.
+
+    :returns: A list of corrections forms suffixed by the type of component
+        race if there is one.
+    """
     recon = get_recon_form(result_form) if result_form.has_recon else None
-    results_general = get_results_for_race_type(result_form,
-                                                RaceType.GENERAL)
-    results_women = get_results_for_race_type(result_form,
-                                              RaceType.WOMEN)
-    results_component = get_results_for_race_type(result_form, None)
+    general = get_results_for_race_type(result_form, RaceType.GENERAL)
+    women = get_results_for_race_type(result_form, RaceType.WOMEN)
+    component = get_results_for_race_type(result_form, None)
 
     # get name of component race type
-    c_name = results_component[0][0].race_type_name\
-        if len(results_component) else None
+    c_name = component[0][0].race_type_name if len(component) else None
 
-    return [recon, results_general, results_women, results_component, c_name]
+    return [recon, general, women, component, c_name]
 
 
 def get_recon_form(result_form):
+    """Build a list of reconciliation form data from the two data entry
+    versions.
+
+    :param result_form: The result form to get reconciliation forms from.
+
+    :returns: A list of tuples of the field from data entry 1, data entry 2,
+        and the name of the field.
+    """
     results = result_form.corrections_reconciliationforms
 
     if results.count() != 2:
@@ -83,17 +113,11 @@ def get_recon_form(result_form):
             raise SuspiciousOperation(_(u"There should be exactly two "
                                         u"reconciliation results."))
 
-    reconciliation_form_1 = recon_form_for_version(
-        results, EntryVersion.DATA_ENTRY_1)
-    reconciliation_form_2 = recon_form_for_version(
-        results, EntryVersion.DATA_ENTRY_2)
+    recon_form_1 = recon_form_for_version(results, EntryVersion.DATA_ENTRY_1)
+    recon_form_2 = recon_form_for_version(results, EntryVersion.DATA_ENTRY_2)
 
-    recon = []
-    for field in reconciliation_form_1:
-        recon.append((field, reconciliation_form_2[field.name],
-                     field.data.__class__.__name__))
-
-    return recon
+    return [(field, recon_form_2[field.name], field.data.__class__.__name__)
+            for field in recon_form_1]
 
 
 def match_forms(result_form):
@@ -102,23 +126,27 @@ def match_forms(result_form):
 
 
 def recon_form_for_version(results, entry_version):
-    return ReconForm(data=model_to_dict(
-        results.filter(entry_version=entry_version)[0]))
+    return ReconForm(data=model_to_dict(results.filter(
+        entry_version=entry_version)[0]))
 
 
 def save_unchanged_final_recon_form(result_form, user):
     if result_form.reconciliationform_exists:
-        updated = _get_recon_form_dict(result_form)
-        _save_final_recon_form(updated, user, result_form)
+        updated = get_recon_form_dict(result_form)
+        save_final_recon_form(updated, user, result_form)
 
 
 def save_recon(post_data, user, result_form):
-    recon_form = get_recon_form(result_form)
+    """Build final reconciliation form from existing and submitted data.
 
+    :param post_data: The form data to retrieve corrections from.
+    :param user: The user to associate with the final reconciliation form.
+    :param result_form: The result form to update reconciliation forms for.
+    """
     corrections = {}
     mismatched = 0
 
-    for v1, v2, _type in recon_form:
+    for v1, v2, _type in get_recon_form(result_form):
         if v1.data != v2.data:
             mismatched += 1
             value = post_data.get(v1.name)
@@ -126,15 +154,16 @@ def save_recon(post_data, user, result_form):
                 if value == 'False':
                     value = False
                 corrections[v1.name] = value
+
     if len(corrections) < mismatched:
         raise ValidationError(
             _(u"Please select correct results for all mis-matched votes."))
 
-    updated = _get_recon_form_dict(result_form)
+    updated = get_recon_form_dict(result_form)
     updated = {k: v for k, v in updated.items() if k not in corrections}
     updated.update(corrections)
 
-    _save_final_recon_form(updated, user, result_form)
+    save_final_recon_form(updated, user, result_form)
 
 
 class CorrectionView(LoginRequiredMixin,
@@ -160,9 +189,7 @@ class CorrectionView(LoginRequiredMixin,
         if form.is_valid():
             barcode = form.cleaned_data['barcode']
             result_form = get_object_or_404(ResultForm, barcode=barcode)
-
-            form = safe_form_in_state(result_form, FormState.CORRECTION,
-                                      form)
+            form = safe_form_in_state(result_form, FormState.CORRECTION, form)
 
             if form:
                 return self.form_invalid(form)
@@ -268,13 +295,14 @@ class CorrectionRequiredView(LoginRequiredMixin,
                     save_component_results(result_form, post_data, user)
                     save_general_results(result_form, post_data, user)
                     save_women_results(result_form, post_data, user)
-
             except ValidationError as e:
                 return self.corrections_response(result_form, u"%s" % e)
             except SuspiciousOperation as e:
                 self.request.session['error_message'] = u"%s" % e
+
                 if result_form.form_state == FormState.DATA_ENTRY_1:
                     result_form.save()
+
                 return redirect(self.failed_url)
             else:
                 result_form.form_state = FormState.QUALITY_CONTROL
