b45cdf1f3341988aa0de7b0d312c7eb995e98088
Jose Marente <jose.marente@crononauta.com>
2014-06-09 09:02:32 +0000

    Adds enable center and station link

--- tally_ho/libs/utils/templates.py
+++ tally_ho/libs/utils/templates.py
@@ -1,17 +1,24 @@
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 
 def getActiveCenterLink(station):
-  buttonHtml = 'Center Disabled'
-  if station.center.active == 1:
-    url = reverse('disable', args=[station.center.code])
-    buttonHtml = '<a href="%s">Disable Center</a>' % url
+    buttonHtml = 'Center disabled'
+    if station.center.active:
+        url = reverse('disable', args=[station.center.code])
+        buttonHtml = '<a href="%s">%s</a>' % (url, unicode(_('Disable Center')))
+    else:
+        url = reverse('enable', args=[station.center.code])
+        buttonHtml = '<a href="%s">%s</a>' % (url, unicode(_('Enable Center')))
 
-  return buttonHtml
+    return buttonHtml
 
 def getActiveStationLink(station):
-  buttonHtml = 'Station Disabled'
-  if station.active == 1:
-    url = reverse('disable', args=[station.center.code, station.station_number])
-    buttonHtml = '<a href="%s">Disable Station</a>' % url
+    buttonHtml = 'Station disabled'
+    if station.active:
+        url = reverse('disable', args=[station.center.code, station.station_number])
+        buttonHtml = '<a href="%s">%s</a>' % (url, unicode(_('Disable Station')))
+    elif station.center.active:
+        url = reverse('enable', args=[station.center.code, station.station_number])
+        buttonHtml = '<a href="%s">%s</a>' % (url, unicode(_('Enable Station')))
 
-  return buttonHtml
+    return buttonHtml
