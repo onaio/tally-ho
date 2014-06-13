0d7370607bed0651587090658a2b5179135575ea
David Casas <david.casas@crononauta.com>
2014-06-13 12:24:20 +0000

    Changed labels in disable_entity template

--- tally_ho/apps/tally/views/super_admin.py
+++ tally_ho/apps/tally/views/super_admin.py
@@ -290,7 +290,7 @@ class DisableEntityView(LoginRequiredMixin,
         stationNumber = kwargs.get('stationNumber')
         centerCode = kwargs.get('centerCode')
 
-        entityName = u'Center' if centerCode else u'Station'
+        entityName = u'Center' if not stationNumber else u'Station'
 
         self.initial = {
             'centerCodeInput': centerCode,
@@ -359,7 +359,8 @@ class RemoveStationView(LoginRequiredMixin,
         form = self.get_form(form_class)
 
         return self.render_to_response(
-            self.get_context_data(form=form))
+            self.get_context_data(form=form, entity=entityName.lower(),
+                                  entityName=entityName))
 
     def post(self, *args, **kwargs):
         form_class = self.get_form_class()
