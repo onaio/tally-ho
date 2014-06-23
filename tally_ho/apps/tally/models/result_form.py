6b7f93dd258de02f4f43e881dbc0d736b6f0cc5c
Jose Marente <jose.marente@crononauta.com>
2014-06-23 13:04:57 +0000

    Adds intake_printed attribute

--- tally_ho/apps/tally/models/result_form.py
+++ tally_ho/apps/tally/models/result_form.py
@@ -156,6 +156,7 @@ class ResultForm(BaseModel):
     skip_quarantine_checks = models.BooleanField(default=False)
     station_number = models.PositiveSmallIntegerField(blank=True, null=True)
     is_replacement = models.BooleanField(default=False)
+    intake_printed = models.BooleanField(default=False)
 
     # Field used in result duplicated list view
     results_duplicated = []
