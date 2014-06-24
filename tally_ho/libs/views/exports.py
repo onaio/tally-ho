75184d90afaef8c7c4a428768e01d7eaa650200e
ewokcillo <azotillo@gmail.com>
2014-06-24 14:13:39 -0400

    fixed division with zero in export results

--- tally_ho/libs/views/exports.py
+++ tally_ho/libs/views/exports.py
@@ -302,7 +302,8 @@ def export_candidate_votes(save_barcodes=False, output_duplicates=True,
             num_stations_completed = final_forms.count()
 
             percent_complete = round(
-                100 * num_stations_completed / num_stations, 3)
+                100 * num_stations_completed / num_stations, 3) if num_stations\
+                else 0
 
             output = OrderedDict({
                 'ballot number': ballot.number,
