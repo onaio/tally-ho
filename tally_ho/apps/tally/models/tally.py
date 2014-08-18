bba00968cefe9bf20ff732b30c8006f92c98d412
Jose Marente <jose.marente@crononauta.com>
2014-08-18 06:29:36 +0000

    Adds disable reason field

--- tally_ho/apps/tally/models/tally.py
+++ tally_ho/apps/tally/models/tally.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from tally_ho.libs.modesl.base_model import BaseModel
+from tally_ho.libs.models.enums.disable_reason import DisableReason
 
 
 class Tally(BaseModel):
@@ -10,3 +11,4 @@ class Tally(BaseModel):
     name = models.CharField(max_length=255, null=False, blank=False)
     active = models.BooleanField(default=True)
     modified_date = models.DateTime(null=False, blank=False)
+    disable_reason = enum.EnumField(DisableReason, null=True)
