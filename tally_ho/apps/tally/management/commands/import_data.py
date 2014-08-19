39dc0546ac0918d7dd125ae81ab7f002da6b232f
Jose Marente <jose.marente@crononauta.com>
2014-08-19 10:01:29 +0000

    Modifies import subconstituencies function in order to work with batch import

--- tally_ho/apps/tally/management/commands/import_data.py
+++ tally_ho/apps/tally/management/commands/import_data.py
@@ -73,72 +73,81 @@ def strip_non_numeric(string):
     except ValueError:
         return None
 
+
+def process_sub_constituency_row(tally, row):
+    if invalid_line(row):
+        next
+
+    row = empty_strings_to_none(row)
+
+    try:
+        code_value, field_office, races, ballot_number_general,\
+            ballot_number_women, number_of_ballots,\
+            ballot_number_component = row[:7]
+
+        code_value = int(code_value)
+        number_of_ballots = number_of_ballots and int(
+            number_of_ballots)
+
+        ballot_component = None
+        ballot_general = None
+        ballot_women = None
+
+        if ballot_number_component:
+            component_race_type = get_component_race_type(
+                ballot_number_component,
+                tally=tally)
+
+            ballot_component, _ = Ballot.objects.get_or_create(
+                number=int(ballot_number_component),
+                race_type=component_race_type,
+                tally=tally)
+
+        if ballot_number_general:
+            ballot_general, _ = Ballot.objects.get_or_create(
+                number=int(ballot_number_general),
+                race_type=RaceType.GENERAL,
+                tally=tally)
+
+        if ballot_number_women:
+            ballot_women, _ = Ballot.objects.get_or_create(
+                number=int(ballot_number_women),
+                race_type=RaceType.WOMEN,
+                tally=tally)
+
+        if number_of_ballots == 2 and not (
+                ballot_general and ballot_women):
+            raise Exception(
+                'Missing ballot data: expected 2 ballots, missing '
+                + ('general' if ballot_number_women else 'women'))
+
+        _, created = SubConstituency.objects.get_or_create(
+            code=code_value,
+            field_office=field_office,
+            races=races,
+            ballot_component=ballot_component,
+            ballot_general=ballot_general,
+            ballot_women=ballot_women,
+            number_of_ballots=number_of_ballots,
+            tally=tally)
+
+    except ValueError:
+        pass
+
+
 def import_sub_constituencies_and_ballots(tally = None, subconst_file = None):
     file_to_parse = subconst_file if subconst_file else open(SUB_CONSTITUENCIES_PATH, 'rU')
+    elements_processed = 0;
 
     with file_to_parse as f:
         reader = csv.reader(f)
         reader.next()  # ignore header
 
         for row in reader:
-            if invalid_line(row):
-                next
+            process_sub_constituency_row(tally, row)
+            elements_processed += 1
 
-            row = empty_strings_to_none(row)
-
-            try:
-                code_value, field_office, races, ballot_number_general,\
-                    ballot_number_women, number_of_ballots,\
-                    ballot_number_component = row[:7]
-
-                code_value = int(code_value)
-                number_of_ballots = number_of_ballots and int(
-                    number_of_ballots)
-
-                ballot_component = None
-                ballot_general = None
-                ballot_women = None
-
-                if ballot_number_component:
-                    component_race_type = get_component_race_type(
-                        ballot_number_component,
-                        tally=tally)
-
-                    ballot_component, _ = Ballot.objects.get_or_create(
-                        number=int(ballot_number_component),
-                        race_type=component_race_type,
-                        tally=tally)
-
-                if ballot_number_general:
-                    ballot_general, _ = Ballot.objects.get_or_create(
-                        number=int(ballot_number_general),
-                        race_type=RaceType.GENERAL,
-                        tally=tally)
-
-                if ballot_number_women:
-                    ballot_women, _ = Ballot.objects.get_or_create(
-                        number=int(ballot_number_women),
-                        race_type=RaceType.WOMEN,
-                        tally=tally)
-
-                if number_of_ballots == 2 and not (
-                        ballot_general and ballot_women):
-                    raise Exception(
-                        'Missing ballot data: expected 2 ballots, missing '
-                        + ('general' if ballot_number_women else 'women'))
-
-                _, created = SubConstituency.objects.get_or_create(
-                    code=code_value,
-                    field_office=field_office,
-                    races=races,
-                    ballot_component=ballot_component,
-                    ballot_general=ballot_general,
-                    ballot_women=ballot_women,
-                    number_of_ballots=number_of_ballots,
-                    tally=tally)
-
-            except ValueError:
-                pass
+    return elements_processed
 
 
 def import_centers(tally = None, centers_file = None):
