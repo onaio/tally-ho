5474b6a7fedd8f5bbd7e4b37e4e1b44eecc84798
David Casas <david.casas@crononauta.com>
2014-08-28 08:25:23 +0000

    changed font-size of center table

--- tally_ho/libs/utils/templates.py
+++ tally_ho/libs/utils/templates.py
@@ -2,9 +2,16 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
-def getEditCenterLink(station):
-    url = reverse('edit-centre', args=[station.center.tally.id, station.center.code])
-    buttonHtml = '<a href="%s" class="btn btn-default btn-small">%s</a>' % (url, unicode(_('Edit Center')))
+def getEditsLink(station):
+    url_center = reverse('edit-centre', args=[station.center.tally.id,
+                                              station.center.code])
+    url_station = reverse('edit-station', args=[station.center.tally.id,
+                                                station.center.code,
+                                                station.station_number])
+
+    template = '<a href="%s" class="btn btn-default btn-small vertical-margin">%s</a><a href="%s" class="btn btn-default btn-small vertical-margin">%s</a>'
+    buttonHtml = template % (url_center, unicode(_('Center')),
+                             url_station, unicode(_('Station')))
 
     return buttonHtml
 
@@ -18,15 +25,6 @@ def getActiveCenterLink(station):
     else:
         url = reverse('enable', args=[station.center.code])
         buttonHtml = '<a href="%s">%s</a>' % (url, unicode(_('Enable Center')))
-
-    return buttonHtml
-
-
-def getEditStationLink(station):
-    url = reverse('edit-station', args=[station.center.tally.id,
-                                        station.center.code,
-                                        station.station_number])
-    buttonHtml = '<a href="%s" class="btn btn-default btn-small">%s</a>' % (url, unicode(_('Edit Station')))
 
     return buttonHtml
 
