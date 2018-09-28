8b087e6c63e8012ba33ed2d7167e4a6f15c4c3d0
pld <peetldee@gmail.com>
2018-09-27 23:32:18 -0400

    give super admin all access

--- tally_ho/libs/views/mixins.py
+++ tally_ho/libs/views/mixins.py
@@ -39,17 +39,11 @@ class GroupRequiredMixin(object):
 
         :returns: True if user is in an allowed group, otherwise False.
         """
-        # super admin skips group check
         user_groups = groups.user_groups(self.request.user)
 
-        if len(listify(allowed_groups)) == 1 and \
-                groups.TALLY_MANAGER in listify(allowed_groups):
-            return groups.TALLY_MANAGER in user_groups
-
-        else:
-            return groups.TALLY_MANAGER in user_groups or\
-                    groups.SUPER_ADMINISTRATOR in user_groups or\
-                    set(listify(allowed_groups)) & set(user_groups)
+        # super admin skips group check
+        return groups.SUPER_ADMINISTRATOR in user_groups or\
+            set(listify(allowed_groups)) & set(user_groups)
 
     def dispatch(self, request, *args, **kwargs):
         self.request = request
