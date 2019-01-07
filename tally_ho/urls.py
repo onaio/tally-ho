633a5c517a25d61ecd5918350d3d4a2e8d3f440f
John Mwashuma <jmwashuma@live.com>
2019-01-07 15:24:54 +0300

    Add url route for creating a new race

--- tally_ho/urls.py
+++ tally_ho/urls.py
@@ -157,6 +157,9 @@ urlpatterns = [
             r'(?P<center_code>(\d+))/(?P<station_number>(\d+))?$',
             super_admin.EnableEntityView.as_view(),
             name='enable'),
+    re_path(r'^super-administrator/create-race/(?P<tally_id>(\d+))/$',
+            super_admin.CreateRaceView.as_view(),
+            name='create-race'),
     re_path(r'^super-administrator/edit-race/(?P<tally_id>(\d+))/'
             r'(?P<id>(\d+))$',
             super_admin.EditRaceView.as_view(),
