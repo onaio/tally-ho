996b71dde1f79079ae87c813320e10ce4ebfc3fe
John Mwashuma <jmwashuma@live.com>
2019-01-24 15:02:14 +0300

    Add confirm reset django form

---
+++ tally_ho/apps/tally/forms/confirm_reset_form.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class ConfirmResetForm(forms.Form):
+    reject_reason = forms.CharField(
+        required=True,
+        label=_("Add comment(s) for rejecting this form"),
+        widget=forms.Textarea(attrs={'cols': 80, 'rows': 5}),
+    )
