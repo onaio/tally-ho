52479f2d1fcd60f498e5db66dfcd9e2379218aef
John Mwashuma <jmwashuma@live.com>
2020-02-17 12:51:08 +0300

    Pass user group text to staff performance metrics template

--- tally_ho/apps/tally/views/reports/staff_performance_metrics.py
+++ tally_ho/apps/tally/views/reports/staff_performance_metrics.py
@@ -1,6 +1,8 @@
+import string
 from django.views.generic import TemplateView
 from guardian.mixins import LoginRequiredMixin
 
+from tally_ho.libs.models.enums.form_state import FormState
 from tally_ho.apps.tally.models.result_form_stats import ResultFormStats
 from tally_ho.libs.permissions import groups
 from tally_ho.libs.views import mixins
@@ -18,7 +20,10 @@ class StaffPerformanceMetricsView(LoginRequiredMixin,
         result_form_stats = ResultFormStats.objects.filter(
             result_form__tally__id=tally_id,
             form_state=form_state)
+        user_group =\
+            string.capwords(FormState(int(form_state)).name.replace("_", " "))
         return self.render_to_response(
             self.get_context_data(
                 tally_id=tally_id,
-                result_form_stats=result_form_stats))
+                result_form_stats=result_form_stats,
+                user_group=user_group))
