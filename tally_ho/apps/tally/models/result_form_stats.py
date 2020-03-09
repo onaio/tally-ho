c677490e5c7c9c12ccf679c67b86a571a3f7f54e
John Mwashuma <jmwashuma@live.com>
2020-03-09 17:24:40 +0300

    Update get time elapsed fuction to return hours

--- tally_ho/apps/tally/models/result_form_stats.py
+++ tally_ho/apps/tally/models/result_form_stats.py
@@ -23,20 +23,24 @@ class ResultFormStats(BaseModel):
 
     @property
     def get_time_elapsed(self):
-        """Calculate time taken to process a result form in minutes and seconds.
+        """Calculate time taken to process a result form in hours,
+        minutes and seconds.
 
         Find the difference between the end time and start time.
         :param start_time: Time the result form started to be processed.
         :param end_time: Time the result form ended to be processed.
 
-        :returns: A dict of rounded time in minutes and seconds.
+        :returns: A dict of rounded time in hours, minutes and seconds.
         """
         one_minute_in_seconds = 60
-        time_elapsed =\
+        minutes, seconds =\
             (divmod((self.end_time - self.start_time).total_seconds(),
                     one_minute_in_seconds))
-        return {'minutes': round(time_elapsed[0]),
-                'seconds':  round(time_elapsed[1])}
+        hours, minutes = divmod(minutes, one_minute_in_seconds)
+
+        return {'hours': round(hours),
+                'minutes': round(minutes),
+                'seconds':  round(seconds)}
 
 
 reversion.register(ResultFormStats)
