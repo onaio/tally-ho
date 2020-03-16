ec0d5e5c7cfe791c59c95a2644894dd339d12762
John Mwashuma <jmwashuma@live.com>
2020-03-16 11:20:26 +0300

    Create test for supervisor approval rate get method

--- tally_ho/apps/tally/tests/views/reports/test_staff_performance_metrics.py
+++ tally_ho/apps/tally/tests/views/reports/test_staff_performance_metrics.py
@@ -22,7 +22,10 @@ class TestStaffPerformanceMetrics(TestBase):
         tally.users.add(self.user)
 
         result_form =\
-            create_result_form(name="Example", tally=tally)
+            create_result_form(
+                name="Example",
+                tally=tally,
+                form_state=FormState.AUDIT)
         start_time = timezone.now()
         minutes = 65.5
         end_time = start_time + timezone.timedelta(minutes=minutes)
@@ -59,3 +62,57 @@ class TestStaffPerformanceMetrics(TestBase):
         self.assertContains(response, "<th>Forms Processed Per Hour</th>")
         self.assertContains(response, f'<td>{self.user.username}</td>')
         self.assertContains(response, f'<td>{processed_forms_per_hour}</td>')
+
+    def test_surpervisors_approvals_get(self):
+        tally = create_tally()
+        tally.users.add(self.user)
+
+        result_form =\
+            create_result_form(
+                name="Example",
+                tally=tally,
+                form_state=FormState.AUDIT)
+        start_time = timezone.now()
+        minutes = 65.5
+        end_time = start_time + timezone.timedelta(minutes=minutes)
+
+        form_processing_time_in_seconds =\
+            (end_time - start_time).total_seconds()
+
+        # Audit supervisor audit review
+        audit_supervisor_user =\
+            self._create_user('audit_supervisor', 'password')
+        self._add_user_to_group(audit_supervisor_user,
+                                groups.AUDIT_SUPERVISOR)
+        create_result_form_stats(
+            processing_time=form_processing_time_in_seconds,
+            user=audit_supervisor_user,
+            result_form=result_form,
+            reviewed_by_supervisor=True,
+            approved_by_supervisor=True
+        )
+
+        request = self._get_request()
+        view = staff_performance_metrics.SupervisorsApprovalsView.as_view()
+        request = self.factory.get('/')
+        request.user = self.user
+        response = view(request, tally_id=tally.pk,)
+
+        self.assertContains(
+            response,
+            "<h1>Supervisors Approvals Rate Report</h1>")
+        self.assertContains(
+            response,
+            "<h3>Audit Supervisors Approval Rate</h3>")
+        self.assertContains(
+            response,
+            "<h3>Tally Managers Approval Rate</h3>")
+        self.assertContains(response, "<th>Number of Approved Forms</th>")
+        self.assertContains(
+            response,
+            "<th>Number of Forms Sent For Review</th>")
+        self.assertContains(
+            response,
+            "<th>Approval Rate</th>")
+        self.assertContains(response, f'<td>1</td>')
+        self.assertContains(response, f'<td>100.0%</td>')
