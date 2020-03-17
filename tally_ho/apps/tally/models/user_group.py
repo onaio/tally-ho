07c9bc8c468b84c4f6b16a37e9a5e567038961bb
John Mwashuma <jmwashuma@live.com>
2020-03-17 17:23:03 +0300

    Add field tally to usergroup and alter group field to protect on delete

--- tally_ho/apps/tally/models/user_group.py
+++ tally_ho/apps/tally/models/user_group.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import Group
+from tally_ho.apps.tally.models.tally import Tally
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from tally_ho.libs.models.base_model import BaseModel
 
 
 class UserGroup(BaseModel):
-    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    group = models.OneToOneField(Group, on_delete=models.PROTECT)
+    tally = models.ForeignKey(Tally,
+                              null=True,
+                              blank=True,
+                              related_name='usergroup',
+                              on_delete=models.PROTECT)
     idle_timeout = models.PositiveIntegerField(
         null=True, blank=True, default=60)
 
