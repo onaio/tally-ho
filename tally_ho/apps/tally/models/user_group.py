5049c4195e052bd03bfbf756f9aa42480be57fa8
John Mwashuma <jmwashuma@live.com>
2020-03-17 16:43:32 +0300

    Create user group model

---
+++ tally_ho/apps/tally/models/user_group.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from tally_ho.libs.models.base_model import BaseModel
+
+
+class UserGroup(BaseModel):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    idle_timeout = models.PositiveIntegerField(
+        null=True, blank=True, default=60)
+
+
+@receiver(post_save, sender=Group)
+def create_user_group(sender, instance, created, **kwargs):
+    if created:
+        UserGroup.objects.create(group=instance)
+
+
+@receiver(post_save, sender=Group)
+def save_user_group(sender, instance, **kwargs):
+    instance.usergroup.save()
