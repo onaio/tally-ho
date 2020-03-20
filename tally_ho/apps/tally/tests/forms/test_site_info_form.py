a16cb209678a5a4fb93a1e507e9f060193dd0c85
John Mwashuma <jmwashuma@live.com>
2020-03-20 12:38:22 +0300

    Create site info model form tests

---
+++ tally_ho/apps/tally/tests/forms/test_site_info_form.py
@@ -0,0 +1,41 @@
+from tally_ho.libs.tests.test_base import TestBase
+from tally_ho.apps.tally.models.site_info import SiteInfo
+from tally_ho.apps.tally.forms.site_info_form import SiteInfoForm
+
+
+class SiteInfoFormTest(TestBase):
+    def setUp(self):
+        self._create_and_login_user()
+
+    def test_blank_data(self):
+        form = SiteInfoForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'user_idle_timeout': ['This field is required.'],
+        })
+
+    def test_invalid_data(self):
+        user_idle_timeout = 'example'
+        form = SiteInfoForm({'user_idle_timeout': user_idle_timeout})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'user_idle_timeout': ['Enter a whole number.'],
+        })
+
+    def test_valid_data(self):
+        user_idle_timeout = 60
+        form = SiteInfoForm({'user_idle_timeout': user_idle_timeout})
+        self.assertTrue(form.is_valid())
+        site_info = form.save()
+        self.assertEqual(site_info.user_idle_timeout, user_idle_timeout)
+
+    def test_site_info_update(self):
+        user_idle_timeout = 60
+        old_form = SiteInfoForm({'user_idle_timeout': user_idle_timeout})
+        old_form.save()
+        new_form = SiteInfoForm({'user_idle_timeout': 50})
+        site_info = new_form.save()
+        site_info_count = SiteInfo.objects.count()
+        self.assertNotEqual(site_info.user_idle_timeout, user_idle_timeout)
+        self.assertEqual(site_info.user_idle_timeout, 50)
+        self.assertEqual(site_info_count, 1)
