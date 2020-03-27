ec982cf0cc269779d1dbbf027302bdafaa1b66e6
John Mwashuma <jmwashuma@live.com>
2020-03-27 12:07:32 +0300

    Refactor code to use session expiry view which stores some session vars after logout

--- tally_ho/libs/middleware/idle_timeout.py
+++ tally_ho/libs/middleware/idle_timeout.py
@@ -1,8 +1,8 @@
 from time import time
 from datetime import datetime, timedelta
 from django.conf import settings
-from django.contrib import auth
 
+from tally_ho.apps.tally.views.profile import session_expiry_logout_view
 from tally_ho.apps.tally.models.site_info import SiteInfo
 
 
@@ -26,9 +26,14 @@ class IdleTimeout(object):
             if last_visit:
                 time_passed = datetime.now() - datetime.fromtimestamp(
                     last_visit)
+                idle_timeout_in_seconds = IDLE_TIMEOUT * 60
+                days = 0
+                microseconds = 0
 
-                if time_passed > timedelta(0, IDLE_TIMEOUT * 60, 0):
-                    auth.logout(request)
+                if time_passed > timedelta(
+                    days, idle_timeout_in_seconds, microseconds
+                ):
+                    session_expiry_logout_view(request)
 
             if request.user.is_authenticated:
                 # if user is not logged out, set the new last_visit
