a2738e3e817dc67d054fa25d55c0d88b8b3ca57c
John Mwashuma <jmwashuma@live.com>
2020-04-09 12:49:34 +0300

    Refactor code and update tests

--- tally_ho/libs/tests/views/reports/test_progress.py
+++ tally_ho/libs/tests/views/reports/test_progress.py
@@ -9,54 +9,25 @@ from tally_ho.libs.reports import progress
 
 class TestProgress(TestBase):
     def setUp(self):
-        self.barcode = 7
         self.center = create_center()
+        self.center_2 = create_center(code=2, office_name='office2')
         self._create_and_login_user()
         self.tally = create_tally()
         self.tally.users.add(self.user)
 
-        create_result_form(
-            center=self.center, station_number=1,
-            barcode=1, serial_number=1, form_state=FormState.UNSUBMITTED)
-        create_result_form(
-            center=self.center, station_number=2,
-            barcode=2, serial_number=2, form_state=FormState.INTAKE)
-        create_result_form(
-            center=self.center, station_number=3,
-            barcode=3, serial_number=3, form_state=FormState.CLEARANCE)
-        create_result_form(
-            center=self.center, station_number=4,
-            barcode=4, serial_number=4, form_state=FormState.DATA_ENTRY_1)
-        create_result_form(
-            center=self.center, station_number=5,
-            barcode=5, serial_number=5, form_state=FormState.DATA_ENTRY_2)
-        create_result_form(
-            center=self.center, station_number=6,
-            barcode=6, serial_number=6, form_state=FormState.CORRECTION)
-        create_result_form(
-            center=self.center,
-            station_number=7,
-            barcode=self.barcode,
-            serial_number=7,
-            form_state=FormState.QUALITY_CONTROL,
-            tally=self.tally)
-        create_result_form(
-            center=self.center, station_number=8,
-            barcode=8, serial_number=8, form_state=FormState.AUDIT)
-
-        self.center_2 = create_center(code=2, office_name='office2')
-
-        create_result_form(
-            center=self.center_2, station_number=9,
-            barcode=9, serial_number=9, form_state=FormState.UNSUBMITTED)
-        create_result_form(
-            center=self.center_2, station_number=10,
-            barcode=10, serial_number=10, form_state=FormState.ARCHIVED)
+        for i in range(1, 11):
+            create_result_form(
+                center=self.center if i >= 2 else self.center_2,
+                station_number=i,
+                tally=self.tally,
+                barcode=i,
+                serial_number=i,
+                form_state=i - 1)
 
         self.assertEqual(ResultForm.objects.count(), 10)
 
     def test_progress_report(self):
-        report = progress.ExpectedProgressReport()
+        report = progress.ExpectedProgressReport(self.tally.id)
         report.queryset = None
         report.filtered_queryset = None
         with self.assertRaises(ImproperlyConfigured):
@@ -65,50 +36,50 @@ class TestProgress(TestBase):
             report.number
 
     def test_expected_progress_report(self):
-        report = progress.ExpectedProgressReport()
+        report = progress.ExpectedProgressReport(self.tally.id)
         self.assertEqual(report.number, 10)
         self.assertEqual(report.total, 10)
         self.assertEqual(report.percentage, 100.0)
 
     def test_intaken_progress_report(self):
-        report = progress.IntakenProgressReport()
-        self.assertEqual(report.number, 8)
+        report = progress.IntakenProgressReport(self.tally.id)
+        self.assertEqual(report.number, 9)
         self.assertEqual(report.total, 10)
-        self.assertEqual(report.percentage, 80)
+        self.assertEqual(report.percentage, 90.0)
 
     def test_archived_progress_report(self):
-        report = progress.ArchivedProgressReport()
+        report = progress.ArchivedProgressReport(self.tally.id)
         self.assertEqual(report.number, 1)
         self.assertEqual(report.total, 10)
         self.assertEqual(report.percentage, 10)
 
     def test_clearance_progress_report(self):
-        report = progress.ClearanceProgressReport()
+        report = progress.ClearanceProgressReport(self.tally.id)
         self.assertEqual(report.number, 1)
         self.assertEqual(report.total, 10)
         self.assertEqual(report.percentage, 10)
 
     def test_audit_progress_report(self):
-        report = progress.AuditProgressReport()
+        report = progress.AuditProgressReport(self.tally.id)
         self.assertEqual(report.number, 1)
         self.assertEqual(report.total, 10)
         self.assertEqual(report.percentage, 10)
 
     def test_not_receieved_progress_report(self):
-        report = progress.NotRecievedProgressReport()
-        self.assertEqual(report.number, 2)
+        report = progress.NotRecievedProgressReport(self.tally.id)
+        self.assertEqual(report.number, 1)
         self.assertEqual(report.total, 10)
-        self.assertEqual(report.percentage, 20)
+        self.assertEqual(report.percentage, 10)
 
     def test_progress_for_office(self):
-        report = progress.ExpectedProgressReport()
-        report = report.for_center_office(self.center_2.office)
-        self.assertEqual(report.number, 2)
-        self.assertEqual(report.total, 2)
+        report = progress.ExpectedProgressReport(self.tally.id)
+        self.assertEqual(report.number, 10)
+        self.assertEqual(report.total, 10)
         self.assertEqual(report.percentage, 100.0)
 
     def test_valid_votes_per_office(self):
-        result_form = ResultForm.objects.get(barcode=self.barcode)
+        result_form = ResultForm.objects.get(
+            form_state=FormState.QUALITY_CONTROL)
         create_reconciliation_form(result_form, self.user)
         report = progress.ValidVotesProgressReport(self.tally.id)
         valid_votes = report.for_center_office(
