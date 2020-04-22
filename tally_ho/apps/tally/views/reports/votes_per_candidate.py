2030f77940c66b37e26d09a381493150cb2db0bb
John Mwashuma <jmwashuma@live.com>
2020-04-22 18:06:23 +0300

    Refactor votes per candidate view to calculate both station and centers votes per candidate

--- tally_ho/apps/tally/views/reports/votes_per_candidate.py
+++ tally_ho/apps/tally/views/reports/votes_per_candidate.py
@@ -1,5 +1,6 @@
 from django.db.models import Q, Sum
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import TemplateView
 
 from django_datatables_view.base_datatable_view import BaseDatatableView
@@ -11,10 +12,10 @@ from tally_ho.libs.permissions import groups
 from tally_ho.libs.views import mixins
 
 
-class StationVotesPerCandidateListDataView(LoginRequiredMixin,
-                                           mixins.GroupRequiredMixin,
-                                           mixins.TallyAccessMixin,
-                                           BaseDatatableView):
+class VotesPerCandidateListDataView(LoginRequiredMixin,
+                                    mixins.GroupRequiredMixin,
+                                    mixins.TallyAccessMixin,
+                                    BaseDatatableView):
     group_required = groups.SUPER_ADMINISTRATOR
     model = Result
     columns = (
@@ -27,13 +28,22 @@ class StationVotesPerCandidateListDataView(LoginRequiredMixin,
 
     def filter_queryset(self, qs):
         tally_id = self.kwargs['tally_id']
-        station_number = self.kwargs['station_number']
-        qs =\
-            qs.filter(active=True,
-                      entry_version=EntryVersion.FINAL,
-                      votes__gt=0,
-                      result_form__tally_id=tally_id,
-                      result_form__station_number=station_number)\
+
+        qs = qs.filter(active=True,
+                       entry_version=EntryVersion.FINAL,
+                       votes__gt=0,
+                       result_form__tally_id=tally_id)
+
+        station_number = self.kwargs.get('station_number', None)
+        center_code = self.kwargs.get('center_code', None)
+
+        if station_number:
+            qs = qs.filter(result_form__station_number=station_number)
+
+        if center_code:
+            qs = qs.filter(result_form__center__code=center_code)
+
+        qs = qs\
             .values('candidate__full_name')\
             .annotate(total_votes_per_candidate=Sum('votes'))
 
@@ -52,24 +62,34 @@ class StationVotesPerCandidateListDataView(LoginRequiredMixin,
                        f'{row["total_votes_per_candidate"]}</td>')
         else:
             return super(
-                StationVotesPerCandidateListDataView, self).render_column(
+                VotesPerCandidateListDataView, self).render_column(
                 row, column)
 
 
-class StationVotesPerCandidateListView(LoginRequiredMixin,
-                                       mixins.GroupRequiredMixin,
-                                       mixins.TallyAccessMixin,
-                                       TemplateView):
+class VotesPerCandidateListView(LoginRequiredMixin,
+                                mixins.GroupRequiredMixin,
+                                mixins.TallyAccessMixin,
+                                TemplateView):
     group_required = groups.SUPER_ADMINISTRATOR
     model = Result
-    template_name = "reports/station_votes_per_candidate.html"
+    template_name = "reports/votes_per_candidate.html"
 
     def get(self, request, *args, **kwargs):
         tally_id = kwargs.get('tally_id')
-        station_number = kwargs.get('station_number')
+        station_number = kwargs.get('station_number', None)
+        center_code = kwargs.get('center_code', None)
+        report_name = None
+
+        if station_number:
+            report_name = _('Station')
+
+        if center_code:
+            report_name = _('Center')
 
         return self.render_to_response(self.get_context_data(
             remote_url=reverse(
-                'staion-votes-per-candidate-list-data', kwargs=kwargs),
+                'votes-per-candidate-list-data', kwargs=kwargs),
             tally_id=tally_id,
-            station_number=station_number))
+            station_number=station_number,
+            center_code=center_code,
+            report_name=report_name))
