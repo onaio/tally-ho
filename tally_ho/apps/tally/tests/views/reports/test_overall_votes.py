fa00b18627c1318cf7b9aaa329d1e15356ea291e
John Mwashuma <jmwashuma@live.com>
2020-04-23 12:10:35 +0300

    Refactor test and test that Center overall votes page is rendered

--- tally_ho/apps/tally/tests/views/reports/test_overall_votes.py
+++ tally_ho/apps/tally/tests/views/reports/test_overall_votes.py
@@ -14,17 +14,14 @@ class TestOverallVotes(TestBase):
         self._create_permission_groups()
         self._create_and_login_user()
         self._add_user_to_group(self.user, groups.TALLY_MANAGER)
+        self.tally = create_tally()
+        self.tally.users.add(self.user)
 
-    def test_station_overall_votes(self):
-        """Test that station overall votes page is rendered"""
-        tally = create_tally()
-        tally.users.add(self.user)
-
-        ballot = create_ballot(tally=tally)
-        center = create_center('12345', tally=tally)
+        ballot = create_ballot(tally=self.tally)
+        center = create_center('12345', tally=self.tally)
         station = create_station(center)
         result_form = create_result_form(
-            tally=tally,
+            tally=self.tally,
             ballot=ballot,
             center=center,
             station_number=station.station_number)
@@ -37,14 +34,30 @@ class TestOverallVotes(TestBase):
             # create duplicate final results
             create_result(result_form, result.candidate, self.user, votes)
 
+    def test_station_overall_votes_get(self):
+        """Test that Station overall votes page is rendered"""
         request = self._get_request()
         view = overall_votes.OverallVotes.as_view()
         request = self.factory.get(
-            f'/reports/internal/station-overall-votes/{tally.pk}')
+            f'/reports/internal/station-overall-votes/{self.tally.pk}')
         request.user = self.user
         response = view(
             request,
-            tally_id=tally.pk,
+            tally_id=self.tally.pk,
             group_name=groups.SUPER_ADMINISTRATOR)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Station Overall Progress")
+
+    def test_center_overall_votes_get(self):
+        """Test that Center overall votes page is rendered"""
+        request = self._get_request()
+        view = overall_votes.OverallVotes.as_view()
+        request = self.factory.get(
+            f'/reports/internal/center-overall-votes/{self.tally.pk}')
+        request.user = self.user
+        response = view(
+            request,
+            tally_id=self.tally.pk,
+            group_name=groups.SUPER_ADMINISTRATOR)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Center Overall Progress")
