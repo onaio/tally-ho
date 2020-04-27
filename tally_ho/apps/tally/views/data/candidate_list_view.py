8805dc0c4c7c541ff3978ffa722767a8da5108a5
John Mwashuma <jmwashuma@live.com>
2020-04-27 17:37:30 +0300

    Get candidates by office id when set

--- tally_ho/apps/tally/views/data/candidate_list_view.py
+++ tally_ho/apps/tally/views/data/candidate_list_view.py
@@ -4,6 +4,7 @@ from django_datatables_view.base_datatable_view import BaseDatatableView
 from guardian.mixins import LoginRequiredMixin
 
 from tally_ho.apps.tally.models.candidate import Candidate
+from tally_ho.libs.reports.progress import get_office_candidates_ids
 from tally_ho.libs.permissions import groups
 from tally_ho.libs.views import mixins
 
@@ -33,8 +34,15 @@ class CandidateListDataView(LoginRequiredMixin,
 
     def filter_queryset(self, qs):
         tally_id = self.kwargs.get('tally_id', None)
+        office_id = self.kwargs.get('office_id', None)
 
-        if tally_id:
+        if tally_id and office_id:
+            candidate_ids =\
+                get_office_candidates_ids(
+                    office_id=office_id, tally_id=tally_id)
+            qs = qs.filter(pk__in=candidate_ids)
+
+        if tally_id and not office_id:
             qs = qs.filter(tally__id=tally_id)
 
         return qs
@@ -49,10 +57,15 @@ class CandidateListView(LoginRequiredMixin,
 
     def get(self, *args, **kwargs):
         # check cache
-        tally_id = kwargs['tally_id']
+        tally_id = self.kwargs.get('tally_id', None)
+        office_id = self.kwargs.get('office_id', None)
+        reverse_url = 'candidate-list-data'
+
+        if office_id:
+            reverse_url = 'candidate-list-data-per-office'
 
         return self.render_to_response(self.get_context_data(
             remote_url=reverse(
-                'candidate-list-data',
-                kwargs={'tally_id': tally_id}),
+                reverse_url,
+                kwargs=kwargs),
             tally_id=tally_id))
