557fd2da5c6e46a7d013b5442d09095fdab57f07
John Mwashuma <jmwashuma@live.com>
2020-05-12 12:36:51 +0300

    Set percentage to 100

--- tally_ho/settings/common.py
+++ tally_ho/settings/common.py
@@ -219,8 +219,8 @@ QUARANTINE_DATA = [
     {'name': 'Trigger 8 - Validate turnout percentage',
      'method': 'pass_turnout_percentage_validation',
      'active': False,
-     'value': 10,
-     'percentage': 90},
+     'value': 100,
+     'percentage': 100},
     {'name':
      'Trigger 9 - Validate percentage of votes per candidate of total votes',
      'method': 'pass_percentage_of_votes_per_candidate_validation',
