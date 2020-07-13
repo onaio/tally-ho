a960408c55317905418018a658a286dfca5e2daa
John Mwashuma <jmwashuma@live.com>
2020-07-13 12:37:54 +0300

    Create region model

---
+++ tally_ho/apps/tally/models/region.py
@@ -0,0 +1,31 @@
+from django.db import models
+import reversion
+
+from tally_ho.apps.tally.models.tally import Tally
+from tally_ho.apps.tally.models.office import Office
+from tally_ho.libs.models.base_model import BaseModel
+
+
+class Region(BaseModel):
+    class Meta:
+        app_label = 'tally'
+        ordering = ['name']
+        unique_together = ('name', 'tally')
+
+    name = models.CharField(max_length=255)
+    office = models.ForeignKey(Office,
+                               null=True,
+                               blank=True,
+                               related_name='regions',
+                               on_delete=models.PROTECT)
+    tally = models.ForeignKey(Tally,
+                              null=True,
+                              blank=True,
+                              related_name='regions',
+                              on_delete=models.PROTECT)
+
+    def __str__(self):
+        return u'%s - %s' % (self.number, self.name)
+
+
+reversion.register(Region)
