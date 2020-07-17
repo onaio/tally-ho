37e778bc544595535ef499a4e36aa18bdeccbb40
John Mwashuma <jmwashuma@live.com>
2020-07-17 16:10:38 +0300

    Add request to get method

--- tally_ho/apps/tally/views/reports/turnout_reports.py
+++ tally_ho/apps/tally/views/reports/turnout_reports.py
@@ -65,7 +65,7 @@ class RegionsTurnoutReportView(LoginRequiredMixin,
     group_required = groups.TALLY_MANAGER
     template_name = 'reports/turnout_report.html'
 
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         tally_id = kwargs['tally_id']
 
         return self.render_to_response(
@@ -83,7 +83,7 @@ class ConstituencyTurnoutReportView(LoginRequiredMixin,
     group_required = groups.TALLY_MANAGER
     template_name = 'reports/turnout_report.html'
 
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         tally_id = kwargs['tally_id']
 
         return self.render_to_response(
