40de1199bbe0105aba7c002350abb162a6208496
John Mwashuma <jmwashuma@live.com>
2020-07-17 14:18:36 +0300

    Create help function for generating turnout report

--- tally_ho/apps/tally/views/reports/turnout_reports.py
+++ tally_ho/apps/tally/views/reports/turnout_reports.py
@@ -10,6 +10,54 @@ from tally_ho.libs.views import mixins
 from tally_ho.libs.models.enums.entry_version import EntryVersion
 
 
+def generate_voters_turnout_report(tally_id, report_column_name):
+    """
+    Genarate voters turnout report by using the final reconciliation
+    form to get voter stats.
+
+    :param tally_id: The reconciliation forms tally.
+    :param report_column_name: The result form report column name.
+
+    returns: The turnout report grouped by the report column name.
+    """
+    turnout_report =\
+        ReconciliationForm.objects.get_registrants_and_votes_type().filter(
+            result_form__tally__id=tally_id,
+            entry_version=EntryVersion.FINAL
+        )\
+        .annotate(
+            name=F(report_column_name))\
+        .values(
+            'name'
+        )\
+        .annotate(
+            number_of_voters_voted=Sum('number_valid_votes'))\
+        .annotate(
+            total_number_of_registrants=Sum('number_of_registrants'))\
+        .annotate(
+            total_number_of_ballots_used=Sum(
+                ExpressionWrapper(F('number_valid_votes') +
+                                  F('number_cancelled_ballots') +
+                                  F('number_unstamped_ballots') +
+                                  F('number_invalid_votes'),
+                                  output_field=IntegerField())))\
+        .annotate(turnout_percentage=ExpressionWrapper(
+            V(100) *
+            F('total_number_of_ballots_used') /
+            F('total_number_of_registrants'),
+            output_field=IntegerField()))\
+        .annotate(male_voters=Coalesce(
+            Sum('number_valid_votes',
+                filter=Q(voters_gender_type=0)),
+            V(0)))\
+        .annotate(female_voters=Coalesce(
+            Sum('number_valid_votes',
+                filter=Q(voters_gender_type=1)),
+            V(0)))
+
+    return turnout_report
+
+
 class RegionsTurnoutReportView(LoginRequiredMixin,
                                mixins.GroupRequiredMixin,
                                TemplateView):
@@ -18,40 +66,6 @@ class RegionsTurnoutReportView(LoginRequiredMixin,
 
     def get(self, *args, **kwargs):
         tally_id = kwargs['tally_id']
-        regions_turnout_report =\
-            ReconciliationForm.objects.get_registrants_and_votes_type().filter(
-                result_form__tally__id=tally_id,
-                entry_version=EntryVersion.FINAL
-            )\
-            .annotate(
-                name=F('result_form__office__region__name'))\
-            .values(
-                'name'
-            )\
-            .annotate(
-                number_of_voters_voted=Sum('number_valid_votes'))\
-            .annotate(
-                total_number_of_registrants=Sum('number_of_registrants'))\
-            .annotate(
-                total_number_of_ballots_used=Sum(
-                    ExpressionWrapper(F('number_valid_votes') +
-                                      F('number_cancelled_ballots') +
-                                      F('number_unstamped_ballots') +
-                                      F('number_invalid_votes'),
-                                      output_field=IntegerField())))\
-            .annotate(turnout_percentage=ExpressionWrapper(
-                V(100) *
-                F('total_number_of_ballots_used') /
-                F('total_number_of_registrants'),
-                output_field=IntegerField()))\
-            .annotate(male_voters=Coalesce(
-                Sum('number_valid_votes',
-                    filter=Q(voters_gender_type=0)),
-                V(0)))\
-            .annotate(female_voters=Coalesce(
-                Sum('number_valid_votes',
-                    filter=Q(voters_gender_type=1)),
-                V(0)))
 
         return self.render_to_response(
             self.get_context_data(
