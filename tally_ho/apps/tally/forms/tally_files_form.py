4f4ea2caaa93bae49b2a67f4265e54db556bbfc6
John Mwashuma <jmwashuma@live.com>
2020-09-15 12:17:51 +0300

    Code clean up

--- tally_ho/apps/tally/forms/tally_files_form.py
+++ tally_ho/apps/tally/forms/tally_files_form.py
@@ -36,15 +36,3 @@ class TallyFilesForm(forms.Form):
                             check_file_size=False,
                             allowed_extensions=ALLOWED_FILE_EXTENTIONS)
     tally_id = forms.IntegerField(widget=forms.HiddenInput())
-
-    def clean(self):
-        cleaned_data = super(TallyFilesForm, self).clean()
-        files = ['subconst_file', 'centers_file',
-                 'candidates_file', 'ballots_order_file', 'result_forms_file']
-        for file in files:
-            file = cleaned_data.get(file, None)
-            file_extension = pathlib.Path(file.name).suffix
-            if file_extension != '.csv':
-                raise forms.ValidationError(
-                    _(f'File extension ({file_extension}) is not supported.'
-                      ' Allowed extensions are .csv'))
