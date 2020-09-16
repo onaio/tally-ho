c16f552c3e7c3260a1a0525743a22cdd2d7ec3be
John Mwashuma <jmwashuma@live.com>
2020-09-17 00:16:07 +0300

    Check that result form csv file columns are correct

--- tally_ho/apps/tally/views/tally_manager.py
+++ tally_ho/apps/tally/views/tally_manager.py
@@ -133,6 +133,8 @@ def import_rows_batch(tally,
     stations_file_name = f'{stations_file_name_prefix}{tally.id}.csv'
     candidates_file_name_prefix = FILE_NAMES_PREFIXS['candidates_file']
     candidates_file_name = f'{candidates_file_name_prefix}{tally.id}.csv'
+    result_forms_file_name_prefix = FILE_NAMES_PREFIXS['result_forms_file']
+    result_forms_file_name = f'{result_forms_file_name_prefix}{tally.id}.csv'
 
     if ballot_file_to_parse:
         with ballot_file_to_parse as f:
@@ -172,6 +174,11 @@ def import_rows_batch(tally,
                         row != settings.CANDIDATE_COLUMN_NAMES:
                     delete_all_tally_objects(tally)
                     error_message = _(u'Invalid candidates file')
+                    return elements_processed, error_message
+                elif file_to_parse_name == result_forms_file_name and\
+                        row != settings.RESULT_FORM_COLUMN_NAMES:
+                    delete_all_tally_objects(tally)
+                    error_message = _(u'Invalid result form file')
                     return elements_processed, error_message
                 else:
                     check_file_column_names = False
