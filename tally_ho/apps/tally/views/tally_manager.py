807f2db231662e7e41f7e7f6bf61198e642c5e84
John Mwashuma <jmwashuma@live.com>
2020-09-17 00:10:23 +0300

    Check that centers csv file columns are correct

--- tally_ho/apps/tally/views/tally_manager.py
+++ tally_ho/apps/tally/views/tally_manager.py
@@ -127,6 +127,8 @@ def import_rows_batch(tally,
 
     subconst_file_name_prefix = FILE_NAMES_PREFIXS['subconst_file']
     subconst_file_name = f'{subconst_file_name_prefix}{tally.id}.csv'
+    centers_file_name_prefix = FILE_NAMES_PREFIXS['centers_file']
+    centers_file_name = f'{centers_file_name_prefix}{tally.id}.csv'
 
     if ballot_file_to_parse:
         with ballot_file_to_parse as f:
@@ -151,6 +153,11 @@ def import_rows_batch(tally,
                         row != settings.SUB_CONSTITUENCY_COLUMN_NAMES:
                     delete_all_tally_objects(tally)
                     error_message = _(u'Invalid sub constituency file')
+                    return elements_processed, error_message
+                elif file_to_parse_name == centers_file_name and\
+                        row != settings.CENTER_COLUMN_NAMES:
+                    delete_all_tally_objects(tally)
+                    error_message = _(u'Invalid centers file')
                     return elements_processed, error_message
                 else:
                     check_file_column_names = False
