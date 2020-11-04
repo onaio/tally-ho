bc69945a6a0e4f8ea1be447abeda0bca8ddfba50
John Mwashuma <jmwashuma@live.com>
2020-11-04 08:17:12 +0300

    Get default storage path

--- tally_ho/libs/views/exports.py
+++ tally_ho/libs/views/exports.py
@@ -34,8 +34,9 @@ def path_with_timestamp(path):
 
 def save_csv_file_and_symlink(csv_file, path):
     new_path = path_with_timestamp(path)
-    default_storage.save(new_path, File(open(csv_file.name, mode='r')))
-    new_path = os.path.realpath(new_path)
+    file_path =\
+        default_storage.save(new_path, File(open(csv_file.name, mode='r')))
+    new_path = default_storage.path(file_path)
 
     try:
         os.symlink(new_path, path)
