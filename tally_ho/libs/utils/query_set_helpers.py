0d26601708a7fc595a595f0ef291a72cbb58a677
John Mwashuma <jmwashuma@live.com>
2021-02-02 10:14:37 +0300

    Refactor candidates votes query helper func

--- tally_ho/libs/utils/query_set_helpers.py
+++ tally_ho/libs/utils/query_set_helpers.py
@@ -1,10 +1,10 @@
+from django.contrib.postgres.aggregates.general import ArrayAgg
 from django.db import transaction, connection
 from django.db.models import ExpressionWrapper, Count, Q, F, FloatField,\
     Func, Subquery, OuterRef, Case, IntegerField, When, Value as V
-from tally_ho.apps.tally.models.tally import Tally
+import ipdb
 from tally_ho.apps.tally.models.result import Result
-from tally_ho.apps.tally.models.result_form import ResultForm
-from tally_ho.apps.tally.models.station import Station
+from tally_ho.apps.tally.models.candidate import Candidate
 from tally_ho.libs.models.enums.entry_version import EntryVersion
 from tally_ho.libs.models.enums.form_state import FormState
 
@@ -27,9 +27,9 @@ def Round(expr, digits=0, output_field=FloatField()):
            function='ROUND'), output_field=output_field)
 
 
-def build_all_candidates_votes_queryset():
+def build_tally_candidates_votes_queryset():
     """
-    Create a queryset for all candidates votes in the available active tallies.
+    Create a queryset for all candidates votes in a tally.
 
     returns: queryset of all candidates votes.
     """
@@ -38,29 +38,6 @@ def build_all_candidates_votes_queryset():
         Func(F('stations_completed'), function='CAST', template=template)
     stations = Func(F('stations'), function='CAST', template=template)
     ew = Round((100 * stations_completed/stations), digits=3)
-
-    station_id_query =\
-        Subquery(
-            Station.objects.filter(
-                tally__id=OuterRef('tally_id'),
-                center__code=OuterRef('center_code'))
-            .values('id')[:1],
-            output_field=IntegerField())
-    result_form_center_code_sub_query =\
-        Subquery(
-            ResultForm.objects.filter(
-                ballot__number=OuterRef('ballot_number'),
-                tally__id=OuterRef('tally_id')).values(
-                    'center__code'
-            )[:1], output_field=IntegerField())
-
-    result_form_center_id_sub_query =\
-        Subquery(
-            ResultForm.objects.filter(
-                ballot__number=OuterRef('ballot_number'),
-                tally__id=OuterRef('tally_id')).values(
-                    'center__id'
-            )[:1], output_field=IntegerField())
 
     candidate_votes_query =\
         Subquery(
@@ -96,32 +73,28 @@ def build_all_candidates_votes_queryset():
                             'candidate_votes'
             )[:1], output_field=IntegerField())
 
-    qs = Tally.objects.filter(active=True)\
-        .values('ballots__candidates__full_name')\
+    qs = Candidate.objects.all()\
+        .values('full_name')\
         .annotate(
-            tally_id=F('id'),
-            ballot_number=F('ballots__number'),
-            candidate_id=F('ballots__candidates__id'),
-            candidate_active=F('ballots__candidates__active'),
+            tally_id=F('tally__id'),
+            candidate_id=F('id'),
+            ballot_number=F('ballot__number'),
+            candidate_active=F('active'),
             stations=Count(
-                'ballots__resultform__set',
-                filter=Q(ballots__resultform__tally__id=F('tally_id'),
-                         ballots__resultform__center__isnull=False,
-                         ballots__resultform__station_number__isnull=False,
-                         ballots__resultform__ballot__isnull=False)
-                ),
-            center_code=result_form_center_code_sub_query,
-            center_id=result_form_center_id_sub_query,
-            station_number=F('ballots__resultform__station_number'),
-            station_id=station_id_query,
+                'ballot__resultform',
+                filter=Q(ballot__resultform__tally__id=F('tally_id'),
+                         ballot__resultform__center__isnull=False,
+                         ballot__resultform__station_number__isnull=False,
+                         ballot__resultform__ballot__isnull=False)
+            ),
             stations_completed=Count(
-                'ballots__resultform__set',
+                'ballot__resultform',
                 filter=Q(
-                    ballots__resultform__tally__id=F('tally_id'),
-                    ballots__resultform__center__isnull=False,
-                    ballots__resultform__station_number__isnull=False,
-                    ballots__resultform__ballot__isnull=False,
-                    ballots__resultform__form_state=FormState.ARCHIVED)
+                    ballot__resultform__tally__id=F('tally_id'),
+                    ballot__resultform__center__isnull=False,
+                    ballot__resultform__station_number__isnull=False,
+                    ballot__resultform__ballot__isnull=False,
+                    ballot__resultform__form_state=FormState.ARCHIVED)
             ),
             votes=candidate_votes_query,
             total_votes=Case(
@@ -145,7 +118,13 @@ def build_all_candidates_votes_queryset():
                 When(stations__gt=0, then=ew),
                 default=V(0),
                 output_field=FloatField()
-            ))
+            ),
+            center_ids=ArrayAgg(
+                'ballot__resultform__center__id',
+                distinct=True),
+            station_numbers=ArrayAgg(
+                'ballot__resultform__station_number',
+                distinct=True))
 
     return qs
 
