94a1b8708f64be285db7555231692bb461d57283
JohnMwashuma <jmwashuma@live.com>
2021-12-07 16:48:17 +0300

    Remove unused import
    Signed-off-by: JohnMwashuma <jmwashuma@live.com>

--- tally_ho/apps/tally/views/data/form_list_view.py
+++ tally_ho/apps/tally/views/data/form_list_view.py
@@ -1,4 +1,4 @@
-from django.db.models import Q, Subquery, OuterRef, IntegerField, Value as V
+from django.db.models import Q, Subquery, OuterRef, IntegerField
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import TemplateView
 from django.urls import reverse
