6de741b447ae17f7f15a18458c49988645bf30b1
JOHNMWASHUMA <jmwashuma@live.com>
2023-04-27 15:15:48 +0300

    Set default language code to en

--- tally_ho/settings/common.py
+++ tally_ho/settings/common.py
@@ -75,7 +75,7 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
