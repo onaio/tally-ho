15b7999d26ff51bccce10add4df25e02ff8337f8
JOHNMWASHUMA <jmwashuma@live.com>
2023-05-08 10:22:24 +0300

    Refactor logic to support updating electrol races and creation of component ballot numbers field

--- tally_ho/apps/tally/management/commands/create_electrol_races.py
+++ tally_ho/apps/tally/management/commands/create_electrol_races.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db import IntegrityError
+from django.db import transaction
 from django.utils.translation import ugettext_lazy
 
 from tally_ho.apps.tally.models.electrol_race import ElectrolRace
@@ -13,17 +15,31 @@ def create_electrol_races(command, electrol_races):
 
     :returns: A queryset of electrol races created.
     """
+    objs = [
+            ElectrolRace(
+                type=electrol_race.get('type'),
+                code=electrol_race.get('code'),
+                ballot_name=electrol_race.get('ballot_name'),
+                component_ballot_numbers=\
+                    electrol_race.get('component_ballot_numbers'),
+            )
+            for electrol_race in electrol_races
+            ]
     try:
-        objs = [
-        ElectrolRace(
-            type=electrol_race['type'],
-            code=electrol_race['code'],
-            ballot_name=electrol_race['ballot_name'],
-        )
-        for electrol_race in electrol_races
-        ]
-        qs = ElectrolRace.objects.bulk_create(objs=objs)
-        return qs
+        return ElectrolRace.objects.bulk_create(objs=objs)
+    except IntegrityError as e:
+        # Do bulk update incase a duplicate key value error is thrown
+        if 'duplicate key value violates unique constraint' in e.args[0]:
+            with transaction.atomic():
+                for obj in objs:
+                    ElectrolRace.objects.filter(
+                        type=obj.type,
+                        code=obj.code).update(
+                        ballot_name=obj.ballot_name,
+                        component_ballot_numbers=obj.component_ballot_numbers)
+        else:
+            command.stdout.write(command.style.ERROR(
+            "Failed to create electrol races, error: {error}".format(error=e)))
     except Exception as e:
         command.stdout.write(command.style.ERROR(
             "Failed to create electrol races, error: {error}".format(error=e)))
