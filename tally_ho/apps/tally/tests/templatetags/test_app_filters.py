e0e81aa89973b7f261fb6f2c3c009d33e79e9aff
Peter Muriuki <pmuriuki98@gmail.com>
2023-05-09 11:54:07 +0300

    Round processed_forms_per_hour ratio to precision of 2

---
+++ tally_ho/apps/tally/tests/templatetags/test_app_filters.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from tally_ho.apps.tally.templatetags.app_filters import forms_processed_per_hour
+
+
+class TestAppFilters(TestCase):
+
+    def test_forms_processed_per_hour(self):
+        self.assertEqual(forms_processed_per_hour(0, 0), 0)
+        self.assertEqual(forms_processed_per_hour(420, 59), 420)
+        self.assertEqual(forms_processed_per_hour(80085, 35119), 8209.4)
