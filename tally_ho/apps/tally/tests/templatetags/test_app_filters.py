054340ebec01de2eaadd045ccf0d2700d197fa53
Peter Muriuki <pmuriuki98@gmail.com>
2023-05-15 09:26:14 +0300

    Fix lint issue

--- tally_ho/apps/tally/tests/templatetags/test_app_filters.py
+++ tally_ho/apps/tally/tests/templatetags/test_app_filters.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 
-from tally_ho.apps.tally.templatetags.app_filters import forms_processed_per_hour
+from tally_ho.apps.tally.templatetags.app_filters import \
+    forms_processed_per_hour
 
 
 class TestAppFilters(TestCase):
-
     def test_forms_processed_per_hour(self):
         self.assertEqual(forms_processed_per_hour(0, 0), 0)
         self.assertEqual(forms_processed_per_hour(420, 59), 420)
