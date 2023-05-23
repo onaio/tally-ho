253c2508b99d40125884eb72f594813b8fd85fa5
JOHNMWASHUMA <jmwashuma@live.com>
2023-05-23 16:51:32 +0300

    Add tally field

--- tally_ho/apps/tally/models/electrol_race.py
+++ tally_ho/apps/tally/models/electrol_race.py
@@ -1,5 +1,6 @@
 from django.db import models
 import reversion
+from tally_ho.apps.tally.models.tally import Tally
 
 from tally_ho.libs.models.base_model import BaseModel
 
@@ -11,6 +12,11 @@ class ElectrolRace(BaseModel):
         unique_together = (('election_level', 'ballot_name'))
     election_level = models.CharField(max_length=256, null=True)
     ballot_name = models.CharField(max_length=256, null=True)
+    tally = models.ForeignKey(Tally,
+                              null=True,
+                              blank=True,
+                              related_name='electrol_races',
+                              on_delete=models.PROTECT)
 
     def __str__(self):
         return u'%s - %s' % (self.election_level, self.ballot_name)
