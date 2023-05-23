1cb8cec155b5547dd419d76cd268e864f600d9dd
Peter Muriuki <pmuriuki98@gmail.com>
2023-05-23 08:26:29 +0300

    Refactor the query params constants

--- tally_ho/apps/tally/views/constants.py
+++ tally_ho/apps/tally/views/constants.py
@@ -1,3 +1,4 @@
 # query search params keys.
-form_state_query_param = 'pending_in_form_state'
+at_state_query_param = 'at_form_state'
+pending_at_state_query_param = 'pending_at_form_state'
 race_type_query_param = 'race_type'
