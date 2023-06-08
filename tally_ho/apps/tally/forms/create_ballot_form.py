55934390842cfcdf5c9238d563f8c9b25ea30d3b
JOHNMWASHUMA <jmwashuma@live.com>
2023-06-08 13:07:19 +0300

    Get electrol races by tally id

--- tally_ho/apps/tally/forms/create_ballot_form.py
+++ tally_ho/apps/tally/forms/create_ballot_form.py
@@ -1,8 +1,9 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelChoiceField, ModelForm
 from tally_ho.apps.tally.forms.fields import RestrictedFileField
 
 from tally_ho.apps.tally.models.ballot import Ballot
+from tally_ho.apps.tally.models.electrol_race import ElectrolRace
 
 
 class CreateBallotForm(ModelForm):
@@ -19,3 +20,9 @@ class CreateBallotForm(ModelForm):
 
         widgets = {"tally": forms.HiddenInput()}
     document = RestrictedFileField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(CreateBallotForm, self).__init__(*args, **kwargs)
+        tally_id = self.initial.get('tally')
+        self.fields['electrol_race'] = ModelChoiceField(
+                queryset=ElectrolRace.objects.filter(tally__id=tally_id))
