1c1b1acd5a87633ca572b968fa0332ccfb80e32a
Peter Muriuki <pmuriuki98@gmail.com>
2023-06-12 13:49:57 +0300

    Update debug toolbar paths registration

--- tally_ho/urls.py
+++ tally_ho/urls.py
@@ -949,8 +949,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
