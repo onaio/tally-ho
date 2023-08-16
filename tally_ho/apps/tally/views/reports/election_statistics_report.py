1eb4b2d6f9cccac2a35a2216015a8bc98d0ab3c4
JOHNMWASHUMA <jmwashuma@live.com>
2023-08-16 16:34:28 +0300

    Fix turnout percentages

--- tally_ho/apps/tally/views/reports/election_statistics_report.py
+++ tally_ho/apps/tally/views/reports/election_statistics_report.py
@@ -190,9 +190,9 @@ def generate_election_statistics(tally_id, election_level, gender=None):
             ballot_election_statistics[
                 'percentage_turnout_in_stations_counted'] =\
                 round(
-                    100 * registrants_in_stations_counted /
-                    voters_in_counted_stations,
-                    2) if voters_in_counted_stations else 0
+                    100 * voters_in_counted_stations /
+                    registrants_in_stations_counted,
+                    2) if registrants_in_stations_counted else 0
             aggregate_ballot_election_statistics['stations_counted'] += \
                 ballot_election_statistics['stations_counted']
             aggregate_ballot_election_statistics[
@@ -212,11 +212,11 @@ def generate_election_statistics(tally_id, election_level, gender=None):
         aggregate_ballot_election_statistics[
             'percentage_turnout_in_stations_counted'] = round(
             100 * aggregate_ballot_election_statistics[
-                'registrants_in_stations_counted'] /
+                'voters_in_counted_stations'] /
             aggregate_ballot_election_statistics[
-                'voters_in_counted_stations'],
+                'registrants_in_stations_counted'],
             2) if aggregate_ballot_election_statistics[
-            'voters_in_counted_stations'] else 0.0
+            'registrants_in_stations_counted'] else 0.0
 
         election_statistics.append(ballot_election_statistics)
     election_statistics.append(aggregate_ballot_election_statistics)
