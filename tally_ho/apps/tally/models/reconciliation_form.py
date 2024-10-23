29dcf7fb615ab23c017da309052753049c46e426
JOHNMWASHUMA <jmwashuma@live.com>
2024-10-23 11:38:26 +0300

    Add number of voter cards in the ballot box field on the recon model

--- tally_ho/apps/tally/models/reconciliation_form.py
+++ tally_ho/apps/tally/models/reconciliation_form.py
@@ -52,6 +52,8 @@ class ReconciliationForm(BaseModel):
         _('Total number of ballots received by the polling station'))
     number_signatures_in_vr = models.PositiveIntegerField(
         _('Number of signatures in the VR'))
+    number_of_voter_cards_in_the_ballot_box = models.PositiveIntegerField(
+        _('Number of voter cards in the ballot box'), default=0)
     number_unused_ballots = models.PositiveIntegerField(
         _('Number of unused ballots'))
     number_spoiled_ballots = models.PositiveIntegerField(
