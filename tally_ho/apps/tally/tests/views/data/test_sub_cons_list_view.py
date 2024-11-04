16e8db8095a43865f893809c326f9fe2d6845fa5
JOHNMWASHUMA <jmwashuma@live.com>
2024-11-04 19:28:28 +0300

    Test SubConstituencyListView

---
+++ tally_ho/apps/tally/tests/views/data/test_sub_cons_list_view.py
@@ -0,0 +1,23 @@
+from django.test import RequestFactory
+
+from tally_ho.apps.tally.views.data import sub_constituency_list_view as views
+from tally_ho.libs.permissions import groups
+from tally_ho.libs.tests.test_base import create_tally, TestBase
+
+
+class TestSubConstituencyListView(TestBase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self._create_permission_groups()
+        self._create_and_login_user()
+        self._add_user_to_group(self.user, groups.SUPER_ADMINISTRATOR)
+
+    def test_ballot_list_view(self):
+        tally = create_tally()
+        tally.users.add(self.user)
+        view = views.SubConstituencyListView.as_view()
+        request = self.factory.get('/')
+        request.user = self.user
+        request.session = {}
+        response = view(request, tally_id=tally.pk)
+        self.assertContains(response, "Sub Constituencies List")
