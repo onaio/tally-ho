3f2a76cea2f6915795b5cc295f530c02e43a293c
JOHNMWASHUMA <jmwashuma@live.com>
2024-11-05 11:22:29 +0300

    Clean up logic for validating recon form required fields

--- tally_ho/apps/tally/static/js/blank_field_check.js
+++ tally_ho/apps/tally/static/js/blank_field_check.js
@@ -1,20 +1,19 @@
 function validate_results(alertMessage) {
     const requiredElements = document.querySelectorAll('.required-input');
-    let allFilled = true;
-    console.log({ requiredElements })
+    let isFormValid = true;
     requiredElements.forEach(el => {
         const inputChild = el.querySelector('input');
         if (inputChild && inputChild.value.trim() === '') {
-            allFilled = false;
+            isFormValid = false;
             el.classList.add('has-error');
         } else if (inputChild) {
             el.classList.remove('has-error');
         }
     });
 
-    if (!allFilled) {
+    if (isFormValid === false) {
         alert(alertMessage);
-        return false;
+        return isFormValid;
     }
-    return true;
+    return isFormValid;
 }
