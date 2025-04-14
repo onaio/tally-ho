2c3e8cfd39db84b584ecf15527715c54fa4f5da8
JOHNMWASHUMA <jmwashuma@live.com>
2025-04-14 15:46:13 +0300

    Update failing test

--- tally_ho/apps/tally/tests/views/test_get_json_results.py
+++ tally_ho/apps/tally/tests/views/test_get_json_results.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from django.urls import reverse
-from tally_ho.libs.models.enums.gender import Gender
-from tally_ho.libs.tests.test_base import TestBase
+from tally_ho.libs.tests.test_base import (
+    TestBase, create_ballot, create_candidate, create_result,
+    create_result_form, create_electrol_race, create_center,
+    create_office, create_sub_constituency)
 from django.http import JsonResponse
-from unittest.mock import patch
 import json
 from tally_ho.libs.permissions import groups
 from tally_ho.libs.tests.test_base import create_tally
 from django.utils import timezone
+from tally_ho.libs.models.enums.form_state import FormState
 
 class GetJSONResultsTest(TestBase):
     def setUp(self):
@@ -18,36 +20,59 @@ class GetJSONResultsTest(TestBase):
         self.tally.users.add(self.user)
         self.url = reverse('download-results')
 
-    @patch('tally_ho.apps.tally.views.reports.administrative_areas_reports.'
-           'results_queryset')
-    @patch('tally_ho.apps.tally.views.reports.administrative_areas_reports.'
-           'get_total_valid_votes_per_electrol_race')
-    def test_get_results_success(
-        self,
-        mock_get_total_valid_votes_per_electrol_race,
-        mock_results_queryset):
-        mock_results_queryset.return_value = [
-            {
-                'candidate_number': 1,
-                'candidate_name': 'Candidate A',
-                'total_votes': 100,
-                'gender': Gender.MALE,
-                'election_level': 'Level 1',
-                'sub_race_type': 'Type A',
-                'order': 1,
-                'ballot_number': '1234',
-                'candidate_status': 'active',
-                'center_code': 'C1',
-                'center_name': 'Center A',
-                'office_number': 'O1',
-                'office_name': 'Office A',
-                'station_number': 'S1',
-                'sub_con_code': 'SUB1',
-                'electrol_race_id': 1,
-                'sub_con_name': 'Sub Con A',
-            }
-        ]
-        mock_get_total_valid_votes_per_electrol_race.return_value = 100
+    def test_get_results_success(self):
+        # Create electrol race
+        electrol_race = create_electrol_race(
+            self.tally,
+            election_level='Level 1',
+            ballot_name='Type A'
+        )
+
+        # Create ballot
+        ballot = create_ballot(
+            self.tally,
+            electrol_race=electrol_race,
+            available_for_release=True
+        )
+
+        # Create candidate
+        candidate = create_candidate(
+            ballot,
+            'Candidate A',
+            tally=self.tally
+        )
+        sub_constituency = create_sub_constituency(
+            name='Sub Con A',
+            tally=self.tally
+        )
+
+        center = create_center(
+            code='1',
+            tally=self.tally,
+            sub_constituency=sub_constituency
+        )
+
+        office = create_office(
+            name='Office A',
+            tally=self.tally
+        )
+
+        # Create result form
+        result_form = create_result_form(
+            ballot=ballot,
+            center=center,
+            office=office,
+            form_state=FormState.ARCHIVED,
+            tally=self.tally
+        )
+
+        # Create result
+        create_result(
+            result_form,
+            candidate,
+            self.user,
+            votes=100
+        )
 
         request_data = json.dumps({'tally_id': self.tally.id})
         response = self.client.get(self.url, {'data': request_data})
@@ -56,24 +81,33 @@ class GetJSONResultsTest(TestBase):
         response_data = json.loads(response.content)
         expected_data = {
             'data': [{
-                'candidate_id': 1,
+                'candidate__candidate_id': 1,
+                'candidate__ballot__number': 1,
+                'candidate__ballot__electrol_race__id': 1,
+                'candidate__ballot__electrol_race__election_level': 'Level 1',
+                'candidate__ballot__electrol_race__ballot_name': 'Type A',
+                'candidate_number': 1,
                 'candidate_name': 'Candidate A',
+                'ballot_number': 1,
                 'total_votes': 100,
-                'gender': Gender.MALE.name,
+                'order': 1,
+                'candidate_status': 'enabled',
+                'electrol_race_id': 1,
                 'election_level': 'Level 1',
                 'sub_race_type': 'Type A',
-                'order': 1,
-                'ballot_number': '1234',
-                'candidate_status': 'active',
-                'center_code': 'C1',
-                'center_name': 'Center A',
-                'office_number': 'O1',
-                'office_name': 'Office A',
-                'station_number': 'S1',
-                'sub_con_code': 'SUB1',
+                'candidate_id': 1,
                 'valid_votes': 100,
-                'sub_con_name': 'Sub Con A',
-            }],
+                'metadata': [{
+                    'barcode': '123456789',
+                    'gender': 'MALE',
+                    'station_number': None,
+                    'center_code': 1,
+                    'center_name': '1',
+                    'office_name': 'Office A',
+                    'office_number': None,
+                    'sub_con_name': 'Sub Con A',
+                    'sub_con_code': 1
+            }]}],
             'created_at': response_data['created_at'],
         }
 
