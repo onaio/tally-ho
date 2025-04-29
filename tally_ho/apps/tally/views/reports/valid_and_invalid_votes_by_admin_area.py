f33bc267456c22ec62f37e46cdf82928aae6f332
JOHNMWASHUMA <jmwashuma@live.com>
2025-04-29 12:20:46 +0300

    Create report for viewing valid and invalid votes by admin area and sub race

---
+++ tally_ho/apps/tally/views/reports/valid_and_invalid_votes_by_admin_area.py
@@ -0,0 +1,258 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import TemplateView
+from guardian.mixins import LoginRequiredMixin
+
+from tally_ho.apps.tally.models import (
+    Station, ResultForm, Result, ReconciliationForm
+    )
+from tally_ho.libs.reports.list_base_data_view import \
+    NoneQsBaseDataView
+from tally_ho.libs.models.enums.entry_version import EntryVersion
+from tally_ho.libs.models.enums.form_state import FormState
+from tally_ho.libs.permissions import groups
+from tally_ho.libs.utils.context_processors import \
+    get_datatables_language_de_from_locale
+from tally_ho.libs.views import mixins
+from django.db.models import ( IntegerField, Subquery, F, OuterRef, Sum, Q )
+from django.db.models.functions import Coalesce
+
+def station_gender_query(
+        tally_id, center_code_filter_name, station_number_filter_name):
+    """
+    Get the gender of the station
+    :param tally_id: The id of the tally
+    :param center_code_filter_name: The name of the center code filter
+    :param station_number_filter_name: The name of the station number filter
+    :return: The gender of the station
+    """
+    return Subquery(
+        Station.objects.filter(
+            tally__id=tally_id,
+            center__code=OuterRef(center_code_filter_name),
+            station_number=OuterRef(station_number_filter_name))
+        .values('gender')[:1],
+        output_field=IntegerField()
+    )
+
+def valid_votes_query(
+        tally_id,
+        admin_level_filter_name,
+        admin_area_name,
+        sub_race_type,
+    ):
+    """
+    Get the valid votes from the result form
+    :param tally_id: The id of the tally
+    :param admin_level_filter_name: The name of the admin level filter
+    :param admin_area_name: The name of the admit area
+    :param sub_race_type: The sub race type
+    :return: The valid votes from the result form
+    """
+    area_filter_map =\
+                {
+                    f"result_form__{admin_level_filter_name}":
+                    admin_area_name
+                }
+    return Result.objects.filter(
+            result_form__tally__id=tally_id,
+            **area_filter_map,
+            result_form__form_state=FormState.ARCHIVED,
+            entry_version=EntryVersion.FINAL,
+            result_form__ballot__electrol_race__ballot_name=\
+                sub_race_type,
+            active=True,
+    ).aggregate(
+        valid_votes=Coalesce(Sum('votes'), 0)
+    ).get('valid_votes')
+
+def invalid_votes_query(
+        tally_id,
+        admin_level_filter_name,
+        admin_area_name,
+        sub_race_type,
+    ):
+    """
+    Get the invalid votes from the reconciliation form
+    :param tally_id: The id of the tally
+    :param admin_level_filter_name: The name of the admin level filter
+    :param admin_area_name: The name of the admin area
+    :param sub_race_type: The sub race type
+    :return: The invalid votes from the reconciliation form
+    """
+    area_filter_map =\
+                {
+                    f"result_form__{admin_level_filter_name}":
+                    admin_area_name
+                }
+    return  ReconciliationForm.objects.filter(
+            result_form__tally__id=tally_id,
+            **area_filter_map,
+            result_form__form_state=FormState.ARCHIVED,
+            entry_version=EntryVersion.FINAL,
+            result_form__ballot__electrol_race__ballot_name=\
+                sub_race_type,
+            active=True,
+    ).aggregate(
+        invalid_votes=Coalesce(Sum('number_invalid_votes'), 0)
+    ).get('invalid_votes')
+
+def get_valid_and_invalid_votes_by_admin_area(
+        tally_id,
+        admin_level_filter_name,
+        admin_level_filter_value,
+    ):
+    """
+    Get the invalid votes from the result form
+    :param tally_id: The id of the tally
+    :param admin_level_filter_name: The name of the admin level filter
+    :param center_code: The center code
+    :param sub_race_type: The sub race type
+    :param station_number: The station number
+    :return: The invalid votes from the result form
+    """
+    base_query = ResultForm.objects.filter(
+        tally__id=tally_id,
+    )
+    if admin_level_filter_value:
+        area_filter_map =\
+                {
+                    f"{admin_level_filter_name}__icontains":
+                    admin_level_filter_value
+                }
+        sub_race_filter_map =\
+                {
+                    "ballot__electrol_race__ballot_name__icontains":
+                    admin_level_filter_value
+                }
+        base_query = base_query.filter(
+            Q(**area_filter_map) |
+            Q(**sub_race_filter_map)
+        )
+    return base_query.annotate(
+        sub_race_type=F('ballot__electrol_race__ballot_name'),
+        admin_area_name=F(admin_level_filter_name),
+    ).values(
+        'admin_area_name',
+        'sub_race_type',
+    ).distinct()
+
+class ValidAndInvalidVotesByAdminAreasDataView(
+    LoginRequiredMixin, mixins.GroupRequiredMixin, mixins.TallyAccessMixin,
+    NoneQsBaseDataView
+    ):
+    group_required = groups.TALLY_MANAGER
+    columns = (
+        "admin_area_name",
+        "sub_race",
+        "total_valid_votes",
+        "total_invalid_votes",
+    )
+
+    def get_initial_queryset(self):
+        tally_id = self.kwargs.get('tally_id')
+        admin_level = self.kwargs.get('admin_level')
+        admin_level_filter_name = None
+        admin_level_filter_value = self.request.POST.get('search[value]')
+        if admin_level == "office":
+            admin_level_filter_name = 'office__name'
+        elif admin_level == "constituency":
+            admin_level_filter_name = 'center__constituency__name'
+        elif admin_level == "sub_constituency":
+            admin_level_filter_name = 'center__sub_constituency__name'
+        else:
+            admin_level_filter_name = 'office__region__name'
+
+        ret_value = []
+
+        for valid_votes_by_sub_race_type in\
+            get_valid_and_invalid_votes_by_admin_area(
+                tally_id,
+                admin_level_filter_name,
+                admin_level_filter_value,
+            ):
+            response = {}
+            response['admin_area_name'] =\
+                valid_votes_by_sub_race_type.get('admin_area_name')
+            response['sub_race'] =\
+                valid_votes_by_sub_race_type.get('sub_race_type')
+            response['total_valid_votes'] = valid_votes_query(
+                tally_id=tally_id,
+                admin_level_filter_name=admin_level_filter_name,
+                admin_area_name=\
+                    valid_votes_by_sub_race_type.get('admin_area_name'),
+                sub_race_type=\
+                    valid_votes_by_sub_race_type.get('sub_race_type'),
+            )
+            response['total_invalid_votes'] = invalid_votes_query(
+                tally_id=tally_id,
+                admin_level_filter_name=admin_level_filter_name,
+                admin_area_name=\
+                    valid_votes_by_sub_race_type.get('admin_area_name'),
+                sub_race_type=\
+                    valid_votes_by_sub_race_type.get('sub_race_type'),
+            )
+            ret_value.append(response)
+
+        return self.ordering(ret_value)
+
+    def get_aggregate(self, data):
+        aggregate = get_aggregate_data(data)
+        return self.prepare_results([aggregate])
+
+
+    def render_column(self, row, column):
+        if column in self.columns:
+            col_value = row[column]
+            return str(
+                '<td class="center" style="padding-left: 10px;">'
+                f'{col_value}</td>'
+                )
+        else:
+            return super(
+                ValidAndInvalidVotesByAdminAreasDataView, self
+                ).render_column(row, column)
+
+def get_aggregate_data(data):
+    aggregate = {}
+    aggregate['admin_area_name'] = "Total"
+    aggregate["sub_race"] = ""
+    aggregate["total_valid_votes"] = sum(
+        entry['total_valid_votes'] for entry in
+        data
+        )
+    aggregate["total_invalid_votes"] = sum(
+        entry['total_invalid_votes'] for entry in
+        data
+        )
+    return aggregate
+
+class ValidAndInvalidVotesByAdminAreasView(
+    LoginRequiredMixin,
+    mixins.GroupRequiredMixin,
+    TemplateView
+    ):
+    group_required = groups.TALLY_MANAGER
+    template_name = 'reports/valid_and_invalid_votes_by_admin_area.html'
+
+    def get(self, request, *args, **kwargs):
+        tally_id = kwargs.get('tally_id')
+        admin_level = kwargs.get('admin_level')
+        language_de = get_datatables_language_de_from_locale(request)
+
+        context = {
+            'tally_id': tally_id,
+            "admin_level": admin_level,
+            'languageDE': language_de,
+            "remote_url":
+                reverse(
+                    'valid-and-invalid-votes-by-adminarea-data',
+                    kwargs=kwargs
+                ),
+            }
+
+        return render(
+            request,
+            'reports/valid_and_invalid_votes_by_admin_area.html',
+            context
+        )
