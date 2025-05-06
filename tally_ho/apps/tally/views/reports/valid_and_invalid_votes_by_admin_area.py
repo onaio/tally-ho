8e2f943d1c0af828fd70fa6f0dffef40e8ad15d3
JOHNMWASHUMA <jmwashuma@live.com>
2025-05-06 14:49:16 +0300

    Fix typo

--- tally_ho/apps/tally/views/reports/valid_and_invalid_votes_by_admin_area.py
+++ tally_ho/apps/tally/views/reports/valid_and_invalid_votes_by_admin_area.py
@@ -187,7 +187,7 @@ class ValidAndInvalidVotesByAdminAreasDataView(
         if column in self.columns:
             col_value = row[column]
             return str(
-                '<td class="center"">'
+                '<td class="center">'
                 f'{col_value}</td>'
                 )
         else:
