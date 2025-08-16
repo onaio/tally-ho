963a49a4430aa7accca1f59bd2e46ffb85f50836
Ukang'a Dickson <ukanga@users.noreply.github.com>
2025-08-17 00:29:51 +0300

    Set autocomplete off on login fields

--- tally_ho/apps/tally/forms/login_form.py
+++ tally_ho/apps/tally/forms/login_form.py
@@ -5,3 +5,5 @@ class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['autofocus'] = 'on'
+        self.fields['username'].widget.attrs['autocomplete'] = 'off'
+        self.fields['password'].widget.attrs['autocomplete'] = 'off'
